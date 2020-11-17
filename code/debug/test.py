65ee665424d45efd2aab59331681d6bf77d959cd
NickAlger <nalger225@gmail.com>
2020-11-16 21:41:31 -0800

    c++ regular grid interpolation works

--- code/debug/test.py
+++ code/debug/test.py
@@ -3,6 +3,7 @@
 # sys.path.append('/Users/gaol/study/pybind/hlibpro/hlibpro-2.8.1/lib')
 
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 import user
 user.bem1d(100)
 
@@ -20,3 +21,12 @@ cc = min_pt.reshape((1,-1)) + np.random.rand(1370,2) * deltas.reshape((1,-1))
 
 ve = user.grid_interpolate(cc, min_pt[0], max_pt[0], min_pt[1], max_pt[1], VG)
 print(ve)
+
+xx = np.linspace(min_pt[0], max_pt[0], nx)
+yy = np.linspace(min_pt[1], max_pt[1], ny)
+RGI = RegularGridInterpolator((xx, yy), VG, method='linear', bounds_error=True, fill_value=0.0)
+ve2 = RGI(cc)
+print(ve2)
+
+err_interp = np.linalg.norm(ve - ve2)
+print('err_interp=', err_interp)
