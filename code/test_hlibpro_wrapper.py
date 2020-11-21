520ba20c3907f6c63df42080bee704e2f8761c6d
NickAlger <nalger225@gmail.com>
2020-11-21 00:29:01 -0800

    good

--- code/test_hlibpro_wrapper.py
+++ code/test_hlibpro_wrapper.py
@@ -6,7 +6,7 @@ import hlibpro_wrapper as hpro
 
 ########    SETUP    ########
 
-mesh = fenics.UnitSquareMesh(85, 95)
+mesh = fenics.UnitSquareMesh(75, 75)
 V = fenics.FunctionSpace(mesh, 'CG', 1)
 dof_coords = V.tabulate_dof_coordinates()
 
