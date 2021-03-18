6cbdb1c4314545f11f39b0a9f983ee8fdf5b59ad
NickAlger <nalger225@gmail.com>
2021-03-18 04:09:51 -0700

    gaussian rbf interpolation with pointwise boundary checking. some hard to find bug

--- code/rbf_interpolation.py
+++ code/rbf_interpolation.py
@@ -6,7 +6,7 @@ import dolfin as dl
 
 class PSFInterpolator:
     def __init__(me, impulse_response_batches, sample_points_batches, mu_fenics_Function, Sigma_fenics_Function,
-                 ellipsoid_tau, drop_tol=1e-2, gaussian_tau=2.0):
+                 ellipsoid_tau, drop_tol=1e-2, gaussian_tau=3.0):
         me.impulse_response_batches = impulse_response_batches
         me.sample_points_batches = sample_points_batches
         me.mu = mu_fenics_Function
@@ -60,7 +60,7 @@ class PSFInterpolator:
         for ii in tqdm(range(me.num_dofs)):
             q = me.dof_coords[ii,:]
             gg = eval_radial_gaussian_kernels_at_point(me.sample_points, me.gaussian_kernel_widths, q)
-            me.relevant_sample_point_inds.append(np.argwhere(gg > me.drop_tol))
+            me.relevant_sample_point_inds.append(list(np.argwhere(gg > me.drop_tol).reshape(-1)))
 
 
         print('Forming mu_at_sample_points')
@@ -79,18 +79,19 @@ class PSFInterpolator:
 
         me.interpolation_matrix_factorizations = dict()
 
-    def eval_kernel(me, target_dof_int_jj, source_dof_ind_ii):
+    def eval_kernel(me, target_dof_ind_jj, source_dof_ind_ii):
         x = me.dof_coords[source_dof_ind_ii, :]
-        y = me.dof_coords[target_dof_int_jj, :]
+        y = me.dof_coords[target_dof_ind_jj, :]
         z = y - x
         return me.eval_convolution_kernel(source_dof_ind_ii, z)
 
     def eval_convolution_kernel(me, dof_ind_ii, shift_z):
         ss_good = list()
-        for s in me.relevant_sample_point_inds[dof_ind_ii]:
-            p = me.sample_points[s,:]
-            mu = me.mu_at_sample_points[s,:]
-            Sigma = me.Sigma_at_sample_points[s,:,:]
+        ss_initial = me.relevant_sample_point_inds[dof_ind_ii]
+        for s in ss_initial:
+            p = me.sample_points[s,:].reshape(me.d)
+            mu = me.mu_at_sample_points[s,:].reshape(me.d)
+            Sigma = me.Sigma_at_sample_points[s,:,:].reshape((me.d, me.d))
             y = p + shift_z
             if point_is_in_ellipsoid(y, mu, Sigma, me.ellipsoid_tau):
                 if me.point_is_in_mesh(y):
@@ -111,17 +112,20 @@ class PSFInterpolator:
         ff = np.array(ff)
 
         if ss_good in me.interpolation_matrix_factorizations:
-            CF = me.interpolation_matrix_factorizations[ss_good]
+            LUF = me.interpolation_matrix_factorizations[ss_good]
         else:
             interpolation_matrix = me.full_interpolation_matrix[np.ix_(ss_good, ss_good)]
-            CF = sla.cho_factor(interpolation_matrix)
-            me.interpolation_matrix_factorizations[ss_good] = CF
+            # print('interpolation_matrix=', interpolation_matrix)
+            LUF = sla.lu_factor(interpolation_matrix)
+            me.interpolation_matrix_factorizations[ss_good] = LUF
 
-        weights = sla.cho_solve(CF, ff)
+        weights = sla.lu_solve(LUF, ff)
+        # print('weights=', weights)
+        # print('ff=', ff)
 
         x = me.dof_coords[dof_ind_ii, :]
-        qq = me.sample_points[ss_good, :]
-        widths = me.gaussian_kernel_widths[ss_good]
+        qq = me.sample_points[list(ss_good), :]
+        widths = me.gaussian_kernel_widths[list(ss_good)]
         phis = eval_radial_gaussian_kernels_at_point(qq, widths, x)
         return np.sum(weights * phis)
 
@@ -153,5 +157,5 @@ def pointcloud_nearest_neighbor_distances(pp):
 
 
 def point_is_in_ellipsoid(p, mu, Sigma, tau):
-    d = mu - p
+    d = (mu - p).reshape(-1)
     return np.dot(d, np.linalg.solve(Sigma, d)) <= tau**2
