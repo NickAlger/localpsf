18487eb926eece0885b59621e3a4f2c1f584fa14
NickAlger <nalger225@gmail.com>
2021-04-16 01:52:29 -0700

    ProductConvolutionOperator.py

--- code/ProductConvolutionOperator.py
+++ code/ProductConvolutionOperator.py
@@ -3,19 +3,42 @@ import numpy as np
 from nalger_helper_functions import *
 
 
-def make_grid2dofs_transfer_matrix(Wk, Fk, pp):
-    C = boxconv(Wk, Fk)
-    inside_inds_k = np.argwhere(point_is_in_box(pp, C.min, C.max)).reshape(-1)
-    T_grid2dof = multilinear_interpolation_matrix(pp[inside_inds_k, :], C.min, C.max, C.shape)
-    return T_grid2dof, inside_inds_k
+def form_product_convolution_operator(WW, FF, V_in, V_out):
+    shape = (V_out.dim(), V_in.dim())
+
+    input_ind_groups = list()
+    TT_dof2grid = list()
+    output_ind_groups = list()
+    TT_grid2dof = list()
+    for k in range(len(WW)):
+        T_dof2grid, input_dof_inds = make_dofs2grid_transfer_matrix(WW[k], V_in)
+        input_ind_groups.append(input_dof_inds)
+        TT_dof2grid.append(T_dof2grid)
+
+        T_grid2dof, output_dof_inds = make_grid2dofs_transfer_matrix(WW[k], FF[k], V_out)
+        output_ind_groups.append(output_dof_inds)
+        TT_grid2dof.append(T_grid2dof)
+
+    return ProductConvolutionOperator(WW, FF, shape, input_ind_groups, output_ind_groups, TT_dof2grid, TT_grid2dof)
 
 
-def make_dofs2grid_transfer_matrix(Wk, V):
-    mesh = V.mesh()
-    pp = V.tabulate_dof_coordinates()
-    inside_mesh_mask = points_inside_mesh(pp, mesh)  # <-- True where point is in mesh, false where point is outside
-    outside_mesh_mask = np.logical_not(inside_mesh_mask)
-    T_dof2grid, nonzero_cols = pointwise_observation_matrix(pp, V, nonzero_columns_only=True)
+def make_grid2dofs_transfer_matrix(Wk, Fk, V_out):
+    if not box_functions_are_conforming(Wk, Fk):
+        raise RuntimeError('BoxFunctions are not conforming')
+
+    pp = V_out.tabulate_dof_coordinates()
+
+    C_min = Wk.min + Fk.min
+    C_max = Wk.max + Fk.max
+    C_shape = tuple(np.array(Wk.shape) + np.array(Fk.shape) - 1)
+    output_dof_inds = np.argwhere(point_is_in_box(pp, C_min, C_max)).reshape(-1)
+    T_grid2dof = multilinear_interpolation_matrix(pp[output_dof_inds, :], C_min, C_max, C_shape)
+    return T_grid2dof, output_dof_inds
+
+
+def make_dofs2grid_transfer_matrix(Wk, V_in):
+    T_dof2grid, input_dof_inds = pointwise_observation_matrix(Wk.gridpoints, V_in, nonzero_columns_only=True)
+    return T_dof2grid, input_dof_inds
 
 
 class ProductConvolutionOperator:
@@ -46,7 +69,7 @@ class ProductConvolutionOperator:
         me.TT_dof2grid = TT_dof2grid
         me.TT_grid2dof = TT_grid2dof
 
-        me.num_pts = len(me.WW)
+        me.num_patches = len(me.WW)
         me.d = WW[0].ndim
         me.shape = shape
 
@@ -56,7 +79,7 @@ class ProductConvolutionOperator:
 
     def matvec(me, u):
         v = np.zeros(me.shape[0], dtype=dtype_max([me.dtype, u.dtype]))
-        for k in range(me.num_pts):
+        for k in range(me.num_patches):
             ii = me.input_ind_groups[k]
             oo = me.output_ind_groups[k]
             Ti = me.TT_dof2grid[k]
@@ -72,7 +95,7 @@ class ProductConvolutionOperator:
 
     def rmatvec(me, v):
         u = np.zeros(me.shape[1], dtype=dtype_max([me.dtype, v.dtype]))
-        for k in range(me.num_pts):
+        for k in range(me.num_patches):
             ii = me.input_ind_groups[k]
             oo = me.output_ind_groups[k]
             Ti = me.TT_dof2grid[k]
