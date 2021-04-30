c7ed66aa76cc279ac441912096bd39a0b296311c
NickAlger <nalger225@gmail.com>
2021-04-30 06:21:12 -0700

    ratioanl

--- code/product_convolution_operator.py
+++ code/product_convolution_operator.py
@@ -442,7 +442,8 @@ def get_W_and_initial_F(w, f, p, mu, Sigma, tau, grid_density_multiplier=1.0, w_
     W_min, W_max, W_shape = conforming_box(W_min0, W_max0, p, h)
     F_min_plus_p, F_max_plus_p, F_shape = conforming_box(F_min_plus_p0, F_max_plus_p0, p, h)
 
-    W_array = eval_fenics_function_on_regular_grid(w, W_min, W_max, W_shape, outside_mesh_fill_value=0.0)
+    W_array = eval_fenics_function_on_regular_grid(w, W_min, W_max, W_shape, outside_mesh_fill_value=None)
+    # W_array = eval_fenics_function_on_regular_grid(w, W_min, W_max, W_shape, outside_mesh_fill_value=0.0)
     F_array = eval_fenics_function_on_regular_grid(f, F_min_plus_p, F_max_plus_p, F_shape, outside_mesh_fill_value=np.nan)
     E = ellipsoid_characteristic_function(F_min_plus_p, F_max_plus_p, F_shape, mu, Sigma, tau)
 
