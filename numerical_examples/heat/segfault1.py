c621511dbf8d689fcdd7f83f0ec235d8661a8e08
NickAlger <nalger225@gmail.com>
2021-11-27 03:07:03 -0600

    segfault1 still happening. not sure if its my code of hlibpro

--- numerical_examples/heat/segfault1.py
+++ numerical_examples/heat/segfault1.py
@@ -61,6 +61,7 @@ PCK.col_batches.add_one_sample_point_batch()
 PCK.col_batches.add_one_sample_point_batch()
 print('mesh_h=', mesh_h, ', num_batches=', PCK.col_batches.num_batches, ', num_sample_points=', PCK.col_batches.num_sample_points)
 
+
 # Hd_pch, extras = make_hmatrix_from_kernel(PCK, make_positive_definite=False, hmatrix_tol=hmatrix_tol) # SEGFAULT
 Hd_pch, extras = make_hmatrix_from_kernel(PCK, make_positive_definite=False, hmatrix_tol=1e-3) # SEGFAULT
 
