8ef0dddd0722257bc29f075e4e56c918e004a54c
NickAlger <nalger225@gmail.com>
2021-12-08 18:11:00 -0600

    error vs krylov iteration plots ok

--- numerical_examples/heat/error_vs_krylov_iter.py
+++ numerical_examples/heat/error_vs_krylov_iter.py
@@ -1,147 +1,189 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from time import time
 
 import hlibpro_python_wrapper as hpro
+from nalger_helper_functions import *
 from localpsf.heat_inverse_problem import *
-from localpsf.product_convolution_hmatrix import product_convolution_hmatrix
+from localpsf.product_convolution_kernel import ProductConvolutionKernel
+from localpsf.product_convolution_hmatrix import make_hmatrix_from_kernel, product_convolution_hmatrix
+from localpsf.visualization import column_error_plot
 from localpsf.morozov_discrepancy import compute_morozov_regularization_parameter
 
+import scipy.sparse.linalg as spla
 
-load_results_from_file = False
-save_results = True
+
+save_data = True
+save_figures = True
 
 save_dir = get_project_root() / 'numerical_examples' / 'heat' / 'error_vs_krylov_iter'
 save_dir.mkdir(parents=True, exist_ok=True)
 
-data_file = str(save_dir / 'data.npz')
-HIP_options_file = save_dir / 'HIP_options.txt'
-options_file = save_dir / 'options.txt'
-plot_file = str(save_dir / 'error_vs_krylov_iter.pdf')
+
+########    OPTIONS    ########
+
+nondefault_HIP_options = {'mesh_h': 2.5e-2} # {'mesh_h': 2e-2}
+
+tau = 2.5
+all_num_batches = [1,3,6,9]
+num_neighbors = 10
+gamma = 1e-5
+sigma_min = 1e-6 # 1e-1 # minimum width of support ellipsoid
+hmatrix_tol=1e-4
 
 
-########    GENERATE OR LOAD RESULTS    ########
+########    SET UP HEAT INVERSE PROBLEM    ########
 
-if not load_results_from_file:
-    ########    OPTIONS    ########
+HIP = HeatInverseProblem(**nondefault_HIP_options)
 
-    nondefault_HIP_options = {'mesh_h': 3e-2}
+########    CONSTRUCT KERNEL    ########
 
-    hmatrix_rtol = 1e-4
-    all_batch_sizes = [1,3,6,9]
+PCK = ProductConvolutionKernel(HIP.V, HIP.V, HIP.apply_Hd_petsc, HIP.apply_Hd_petsc,
+                               10, 10,
+                               tau_rows=tau, tau_cols=tau,
+                               num_neighbors_rows=num_neighbors,
+                               num_neighbors_cols=num_neighbors,
+                               symmetric=True,
+                               gamma=gamma,
+                               sigma_min=sigma_min)
 
-    options = {'hmatrix_rtol' : hmatrix_rtol,
-               'all_batch_sizes' : all_batch_sizes}
+########    CREATE HMATRICES    ########
 
-
-    ########    SET UP HEAT INVERSE PROBLEM    ########
-
-    HIP = HeatInverseProblem(**nondefault_HIP_options)
-
-
-    ########    BUILD PC-HMATRIX APPROXIMATIONS FOR A VARIETY OF BATCH SIZES    ########
-
-    all_Hd_hmatrix = list()
-    all_extras = list()
-    for k in all_batch_sizes:
-        Hd_hmatrix, extras = product_convolution_hmatrix(HIP.V, HIP.V, HIP.apply_Hd_petsc, HIP.apply_Hd_petsc, k,
-                                                         hmatrix_tol=hmatrix_rtol, make_positive_definite=True,
-                                                         return_extras=True, grid_density_multiplier=0.5)
-        all_Hd_hmatrix.append(Hd_hmatrix)
-        all_extras.append(extras)
+A_pch, extras = make_hmatrix_from_kernel(PCK, make_positive_definite=True, hmatrix_tol=hmatrix_tol)
 
 
-    ########    COMPUTE REGULARIZATION PARAMETER VIA MOROZOV DISCREPANCY PRINCIPLE    ########
+########    SET UP REGULARIZATION OPERATOR AND INVERSE PROBLEM SOLVER    ########
 
-    best_Hd_hmatrix = all_Hd_hmatrix[-1]
+R0_hmatrix = hpro.build_hmatrix_from_scipy_sparse_matrix(HIP.R0_scipy, A_pch.bct)
 
-    R0_hmatrix = hpro.build_hmatrix_from_scipy_sparse_matrix(HIP.R0_scipy, best_Hd_hmatrix.bct)
-
-    def solve_inverse_problem(a_reg):
-        HIP.regularization_parameter = a_reg
-
-        g0_numpy = HIP.g_numpy(np.zeros(HIP.N))
-        H_hmatrix = Hd_hmatrix + a_reg * R0_hmatrix
-        iH_hmatrix = H_hmatrix.inv()
-
-        u0_numpy, info, residuals = custom_cg(HIP.H_linop, -g0_numpy,
-                                              M=iH_hmatrix.as_linear_operator(),
-                                              tol=1e-10, maxiter=500)
-
-        u0 = dl.Function(HIP.V)
-        u0.vector()[:] = u0_numpy
-
-        return u0.vector() # dolfin Vector
-
-    a_reg_morozov = compute_morozov_regularization_parameter(solve_inverse_problem,
-                                                             HIP.morozov_discrepancy,
-                                                             HIP.noise_Mnorm)
-
-
-    ########    KRYLOV CONVERGENCE FOR MOROZOV REGULARIZATION PARAMETER    ########
-
-    HIP.regularization_parameter = a_reg_morozov
-
-    Hd_hmatrix = all_Hd_hmatrix[-1]
-    H_hmatrix = Hd_hmatrix + a_reg_morozov * R0_hmatrix
-    iH_hmatrix = H_hmatrix.inv()
+def solve_inverse_problem(a_reg):
+    HIP.regularization_parameter = a_reg
 
     g0_numpy = HIP.g_numpy(np.zeros(HIP.N))
-    u0_numpy, _, _ = custom_cg(HIP.H_linop, -g0_numpy, M=iH_hmatrix.as_linear_operator(), tol=1e-11,
-                               maxiter=1000, track_residuals=True)
+    H_hmatrix = A_pch + a_reg * R0_hmatrix
+    iH_hmatrix = H_hmatrix.inv()
 
-    _, _, errors_reg_morozov = custom_cg(HIP.H_linop, -g0_numpy, M=HIP.solve_R_linop, tol=1e-10, maxiter=1000,
-                                         x_true=u0_numpy, track_residuals=False)
+    u0_numpy, info, residuals = custom_cg(HIP.H_linop, -g0_numpy,
+                                          M=iH_hmatrix.as_linear_operator(),
+                                          tol=1e-10, maxiter=500)
 
-    _, _, errors_none_morozov = custom_cg(HIP.H_linop, -g0_numpy, tol=1e-10, maxiter=1000,
-                                          x_true=u0_numpy, track_residuals=False)
+    u0 = dl.Function(HIP.V)
+    u0.vector()[:] = u0_numpy
 
-    all_errors_hmatrix_morozov = list()
-    for kk_batch in range(len(all_batch_sizes)):
-        Hd_hmatrix = all_Hd_hmatrix[kk_batch]
-        H_hmatrix = Hd_hmatrix + a_reg_morozov * R0_hmatrix
-        iH_hmatrix = H_hmatrix.inv()
-        _, _, errors_hmatrix_morozov = custom_cg(HIP.H_linop, -g0_numpy, M=iH_hmatrix.as_linear_operator(),
-                                                 tol=1e-10, maxiter=1000,
-                                                 x_true=u0_numpy, track_residuals=False)
-        all_errors_hmatrix_morozov.append(errors_hmatrix_morozov)
-
-    if save_results:
-        # Save HIP_options
-        with open(HIP_options_file, 'w') as f:
-            print(HIP.options, file=f)
-
-        # Save options
-        with open(options_file, 'w') as f:
-            print(options, file=f)
-
-        # Save data
-        np.savez(data_file,
-                 errors_reg_morozov=errors_reg_morozov,
-                 errors_none_morozov=errors_none_morozov,
-                 all_errors_hmatrix_morozov=all_errors_hmatrix_morozov,
-                 a_reg_morozov=a_reg_morozov)
-else:
-    data = np.load(data_file)
-    errors_reg_morozov = data['errors_reg_morozov']
-    errors_none_morozov = data['errors_none_morozov']
-    all_errors_hmatrix_morozov = data['all_errors_hmatrix_morozov']
-    a_reg_morozov = data['a_reg_morozov']
-    all_batch_sizes = data['all_batch_sizes']
+    return u0.vector()  # dolfin Vector
 
 
-########    MAKE FIGURE    ########
+########    FIND REGULARIZATION PARAMETER VIA MOROZOV DISCREPANCY PRINCIPLE    ########
+
+a_reg_morozov = compute_morozov_regularization_parameter(solve_inverse_problem,
+                                                         HIP.morozov_discrepancy,
+                                                         HIP.noise_Mnorm)
+
+
+
+########    SOLVE INVERSE PROBLEM VIA CG WITH DIFFERENT PRECONDITIONERS    ########
+
+HIP.regularization_parameter = a_reg_morozov
+
+g0_numpy = HIP.g_numpy(np.zeros(HIP.N))
+H_hmatrix = A_pch + a_reg_morozov * R0_hmatrix
+iH_hmatrix = H_hmatrix.inv()
+
+u0_numpy, info, residuals = custom_cg(HIP.H_linop, -g0_numpy,
+                                      M=iH_hmatrix.as_linear_operator(),
+                                      tol=1e-11, maxiter=500)
+
+_, _, residuals_None, errors_None = custom_cg(HIP.H_linop, -g0_numpy,
+                                              x_true=u0_numpy,
+                                              tol=1e-10, maxiter=500)
+
+_, _, residuals_Reg, errors_Reg = custom_cg(HIP.H_linop, -g0_numpy,
+                                            x_true=u0_numpy,
+                                            M=HIP.solve_R_linop,
+                                            tol=1e-10, maxiter=1000)
+
+
+all_residuals_PCH = list()
+all_errors_PCH = list()
+for num_batches in all_num_batches:
+    print('num_batches=', num_batches)
+    PCK = ProductConvolutionKernel(HIP.V, HIP.V, HIP.apply_Hd_petsc, HIP.apply_Hd_petsc,
+                                   num_batches, num_batches,
+                                   tau_rows=tau, tau_cols=tau,
+                                   num_neighbors_rows=num_neighbors,
+                                   num_neighbors_cols=num_neighbors,
+                                   symmetric=True,
+                                   gamma=gamma,
+                                   sigma_min=sigma_min)
+
+    A_pch, extras = make_hmatrix_from_kernel(PCK, make_positive_definite=True, hmatrix_tol=hmatrix_tol)
+
+    H_hmatrix = A_pch + a_reg_morozov * R0_hmatrix
+    iH_hmatrix = H_hmatrix.inv()
+
+    _, _, residuals_PCH, errors_PCH = custom_cg(HIP.H_linop, -g0_numpy,
+                                                x_true=u0_numpy,
+                                                M=iH_hmatrix.as_linear_operator(),
+                                                tol=1e-10, maxiter=500)
+
+    all_residuals_PCH.append(residuals_PCH)
+    all_errors_PCH.append(errors_PCH)
+
+
+########    SAVE DATA    ########
+
+all_num_batches = np.array(all_num_batches)
+residuals_None = np.array(residuals_None)
+errors_None = np.array(errors_None)
+residuals_Reg = np.array(residuals_Reg)
+errors_Reg = np.array(errors_Reg)
+all_residuals_PCH = [np.array(r) for r in all_residuals_PCH]
+all_errors_PCH = [np.array(e) for e in all_errors_PCH]
+
+if save_data:
+    np.savetxt(save_dir / 'all_num_batches.txt', all_num_batches)
+    np.savetxt(save_dir / 'residuals_None.txt', residuals_None)
+    np.savetxt(save_dir / 'errors_None.txt', errors_None)
+    np.savetxt(save_dir / 'residuals_Reg.txt', residuals_Reg)
+    np.savetxt(save_dir / 'errors_Reg.txt', errors_Reg)
+    for k in range(len(all_num_batches)):
+        np.savetxt(save_dir / ('all_residuals_PCH' + str(all_num_batches[k]) + '.txt'), all_residuals_PCH[k])
+        np.savetxt(save_dir / ('all_errors_PCH' + str(all_num_batches[k]) + '.txt'), all_errors_PCH[k])
+
+
+########    MAKE FIGURES    ########
 
 plt.figure()
-plt.semilogy(errors_reg_morozov)
-plt.semilogy(errors_none_morozov)
-for errors_hmatrix_morozov in all_errors_hmatrix_morozov:
-    plt.semilogy(errors_hmatrix_morozov)
-plt.xlabel('Conjugate gradient iteration')
-plt.ylabel(r'$\frac{\|u_0 - u_0^*\|}{\|u_0^*\|}$')
-plt.title('Convergence of conjugate gradient')
-plt.legend(['Reg', 'None'] + ['PCH'+str(nB) for nB in all_batch_sizes])
+plt.semilogy(errors_Reg)
+plt.semilogy(errors_None)
+legend = ['Reg', 'None']
+for k in range(len(all_num_batches)):
+    plt.semilogy(all_errors_PCH[k])
+    legend.append('PCH ' + str(all_num_batches[k]))
 
-plt.show()
+plt.xlabel('Iteration')
+plt.ylabel('relative l2 error')
+plt.title('Relative error vs. Krylov iteration')
+plt.legend(legend)
 
-if save_results:
-    plt.savefig(plot_file, bbox_inches='tight', dpi=100)
+if save_figures:
+    plt.savefig(save_dir / 'error_vs_krylov_iter.pdf', bbox_inches='tight', dpi=100)
+
+
+plt.figure()
+plt.semilogy(residuals_Reg)
+plt.semilogy(residuals_None)
+legend = ['Reg', 'None']
+for k in range(len(all_num_batches)):
+    plt.semilogy(all_residuals_PCH[k])
+    legend.append('PCH ' + str(all_num_batches[k]))
+
+plt.xlabel('Iteration')
+plt.ylabel('relative residual')
+plt.title('Relative residual vs. Krylov iteration')
+plt.legend(legend)
+
+if save_figures:
+    plt.savefig(save_dir / 'relative_residual_vs_krylov_iter.pdf', bbox_inches='tight', dpi=100)
+
