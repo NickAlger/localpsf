fc2d8d937253cb2f531615d1e48405e57a61f68b
NickAlger <nalger225@gmail.com>
2022-04-28 20:11:35 -0500

    target lower part of the spectrum with small k (k=1 or 2)

--- numerical_examples/heat/error_vs_krylov_iter.py
+++ numerical_examples/heat/error_vs_krylov_iter.py
@@ -22,6 +22,22 @@ save_dir = get_project_root() / 'numerical_examples' / 'heat' / 'error_vs_krylov
 save_dir.mkdir(parents=True, exist_ok=True)
 
 
+#
+
+class LinopWithCounter:
+    def __init__(me, linop):
+        me.counter = 0
+        me.linop = linop
+        me.shape = linop.shape
+        me.dtype = linop.dtype
+
+    def matvec(me, x):
+        me.counter+=1
+        print('counter=', me.counter)
+        return me.linop.matvec(x)
+
+#
+
 ########    OPTIONS    ########
 
 nondefault_HIP_options = {'mesh_h': 2.5e-2} # {'mesh_h': 2e-2}
@@ -63,7 +79,7 @@ A_hmatrix = A_pch_nonsym
 
 a_factor=2.0
 b_factor=0.0
-k=5
+k=2
 rtol = hpro.default_rtol
 atol = hpro.default_atol
 # rtol = 1e-5 # 1e-14
@@ -89,71 +105,360 @@ lambda_min = np.min(ee_SA)
 if display_progress:
     print('lambda_min=', lambda_min)
 
-B = (A_hmatrix * (0.5/np.abs(lambda_min))).add_identity().inv(rtol=rtol, atol=atol)
-# B = (A_hmatrix * (0.5/np.abs(lambda_min)))
-# B.visualize('BBB')
+# B = (A_hmatrix * (0.5/np.abs(lambda_min))).add_identity().inv(rtol=rtol, atol=atol)
+# # B = (A_hmatrix * (0.5/np.abs(lambda_min)))
+# # B.visualize('BBB')
 
-ee_B, _ = spla.eigsh(B.as_linear_operator(), k=1, which='LA')
-ee_B
+# ee_B, _ = spla.eigsh(B.as_linear_operator(), k=1, which='LA')
+# ee_B
 
 if display_progress:
     print('Setting up operator T = (2*A - (b+a) I) / (b-a)')
-a = 1.0
-b = 4.0
+a_minus = -1.5 * np.abs(lambda_min)
+b_minus = 0.0
+a_plus = 0.0
+b_plus = 1.5 * np.abs(lambda_max)
+
+# a = 1.0
+# b = 4.0
 # a = lambda_min * a_factor
 # b = lambda_min * b_factor
 
 if display_progress:
-    scaling_at_one = 1. / (1. + ((2 - (b + a)) / (b - a)) ** (2 ** k))
-    print('scaling_at_one=', scaling_at_one)
+    plus_scaling_at_lambda_min = 1. / (1. + ((2.0 * lambda_min - (b_plus + a_plus)) / (b_plus - a_plus)) ** (2 ** k))
+    print('plus_scaling_at_lambda_min=', plus_scaling_at_lambda_min)
+
+    plus_scaling_at_zero = 1. / (1. + ((2.0*0.0 - (b_plus + a_plus)) / (b_plus - a_plus)) ** (2 ** k))
+    print('plus_scaling_at_zero=', plus_scaling_at_zero)
+
+    plus_scaling_at_lambda_max = 1. / (1. + ((2.0 * lambda_max - (b_plus + a_plus)) / (b_plus - a_plus)) ** (2 ** k))
+    print('plus_scaling_at_lambda_max=', plus_scaling_at_lambda_max)
+
+    minus_scaling_at_lambda_min = 1. / (1. + ((2.0 * lambda_min - (b_minus + a_minus)) / (b_minus - a_minus)) ** (2 ** k))
+    print('minus_scaling_at_lambda_min=', minus_scaling_at_lambda_min)
+
+    minus_scaling_at_zero = 1. / (1. + ((2.0 * 0.0 - (b_minus + a_minus)) / (b_minus - a_minus)) ** (2 ** k))
+    print('minus_scaling_at_zero=', minus_scaling_at_zero)
+
+    minus_scaling_at_lambda_max = 1. / (1. + ((2.0 * lambda_max - (b_minus + a_minus)) / (b_minus - a_minus)) ** (2 ** k))
+    print('minus_scaling_at_lambda_max=', minus_scaling_at_lambda_max)
     # scaling_at_zero = 1. / (1. + ((b + a) / (b - a)) ** (2 ** k))
     # print('scaling_at_zero=', scaling_at_zero)
 
-# T = A_hmatrix.copy()
-T = B.copy()
-T = (T * 2.0).add_identity(s=-(b + a)) * (1.0 / (b - a))
+A_dense = build_dense_matrix_from_matvecs(A_hmatrix.matvec, A_hmatrix.shape[1])
+T_plus_dense =  (2.0*A_dense - (b_plus +a_plus) *np.eye(A_dense.shape[0])) / (b_plus  - a_plus)
+T_minus_dense = (2.0*A_dense - (b_minus+a_minus)*np.eye(A_dense.shape[0])) / (b_minus - a_minus)
 
-ee_T, _ = spla.eigsh(T.as_linear_operator(), k=1, which='LM')
-T_scaling_factor = np.max(np.abs(ee_T))
-if display_progress:
-    print('T_scaling_factor=', T_scaling_factor)
-
-T_hat = T * (1. / T_scaling_factor)
-
-if display_progress:
-    print('computing T^(2^k)')
 for ii in range(k):
+    T_plus_dense = T_plus_dense @ T_plus_dense
+    T_minus_dense = T_minus_dense @ T_minus_dense
+
+Pi_numerator_dense = T_minus_dense - T_plus_dense
+
+Pi_denominator_dense = T_minus_dense + T_plus_dense + 2.0 * np.eye(T_minus_dense.shape[0])
+
+Pi_dense = np.linalg.solve(Pi_denominator_dense, Pi_numerator_dense)
+
+A_plus_dense = Pi_dense @ A_dense
+
+ee_Pi_dense, uu_Pi_dense = sla.eigh(Pi_dense)
+
+plt.figure()
+plt.plot(ee_Pi_dense)
+
+ee_A_dense, uu_A_dense = sla.eigh(A_dense)
+
+ee_A_plus_dense, uu_A_plus_dense = sla.eigh(A_plus_dense)
+
+ray_plus_dense = np.sum(uu_A_dense * (A_plus_dense @ uu_A_dense), axis=0)
+
+plt.figure()
+plt.plot(ee_A_dense)
+plt.plot(ee_A_plus_dense)
+plt.plot(ray_plus_dense)
+
+# if display_progress:
+#     print('inverting spectral projector denominator: Pi_denom^-1')
+# iPi_denominator = Pi_denominator.inv(rtol=rtol, atol=atol)
+#
+# if display_progress:
+#     print('computing spectral projector: Pi_num * Pi_denom^-1')
+# Pi = Pi_numerator * iPi_denominator
+
+# T = A_hmatrix.copy()
+
+
+
+
+
+############################33
+################################3
+####################################
+
+k_plus = 2
+k_minus = 2
+rtol = hpro.default_rtol
+atol = hpro.default_atol
+
+T_plus = A_hmatrix.copy()
+T_plus = (T_plus * 2.0).add_identity(s=-(b_plus + a_plus)) * (1.0 / (b_plus - a_plus))
+
+T_minus = A_hmatrix.copy()
+T_minus = (T_minus * 2.0).add_identity(s=-(b_minus + a_minus)) * (1.0 / (b_minus - a_minus))
+
+if display_progress:
+    print('computing T_plus^(2^k)')
+for ii in range(k_plus):
     if display_progress:
-        print('computing T^(2^' + str(ii + 1) + ') = T^(2^' + str(ii) + ') * T^(2^' + str(ii) + ')')
-    T_hat = hpro.h_mul(T_hat, T_hat,
+        print('computing T_plus^(2^' + str(ii + 1) + ') = T_plus^(2^' + str(ii) + ') * T_plus^(2^' + str(ii) + ')')
+    T_plus = hpro.h_mul(T_plus, T_plus,
               rtol=rtol, atol=atol,
               display_progress=display_progress)
-    T_scaling_factor = T_scaling_factor*T_scaling_factor
-
-T = T_hat * T_scaling_factor
 
 if display_progress:
-    print('computing spectral projector Pi = I / (I + T^(2^k))')
-Pi = T.add_identity().inv(rtol=rtol, atol=atol, display_progress=display_progress)
+    print('computing T_minus^(2^k)')
+for ii in range(k_minus):
+    if display_progress:
+        print('computing T_minus^(2^' + str(ii + 1) + ') = T_minus^(2^' + str(ii) + ') * T_minus^(2^' + str(ii) + ')')
+    T_minus = hpro.h_mul(T_minus, T_minus,
+              rtol=rtol, atol=atol,
+              display_progress=display_progress)
+
+# if display_progress:
+#     print('computing Pi_plus = (I + T_plus^(2^k))^-1')
+# Pi_plus = T_plus.add_identity().inv(rtol=rtol, atol=atol)
+#
+# if display_progress:
+#     print('computing Pi_minus = (I + T_minus^(2^k))^-1')
+# Pi_minus = T_minus.add_identity().inv(rtol=rtol, atol=atol)
+#
+# if display_progress:
+#     print('computing Pi_num = Pi_plus - Pi_minus')
+# Pi_num = Pi_plus - Pi_minus
+#
+# if display_progress:
+#     print('computing iPi_denom = (Pi_plus + Pi_minus)^(-1)')
+# iPi_denom = (Pi_plus + Pi_minus).inv(rtol=rtol, atol=atol)
+#
+# if display_progress:
+#     print('computing Pi = Pi_num * iPi_denom')
+# Pi = hpro.h_mul(Pi_num, iPi_denom,
+#                 rtol=rtol, atol=atol,
+#                 display_progress=display_progress)
+#
+# Pi_num_dense = build_dense_matrix_from_matvecs(Pi_num.matvec, Pi_num.shape[1])
+# iPi_denom_dense = build_dense_matrix_from_matvecs(iPi_denom.matvec, iPi_denom.shape[1])
+# Pi_dense = build_dense_matrix_from_matvecs(Pi.matvec, Pi.shape[1])
+#
+# ee_Pi_dense, uu_Pi_dense = sla.eigh(Pi_dense)
 
 if display_progress:
-    print('computing A_minus = Pi * A')
-A_minus = hpro.h_mul(Pi, A_hmatrix,
+    print('computing spectral projector numerator Pi_num = T_minus^(2^k) - T_plus^(2^k)')
+# Pi_numerator = T_minus - T_plus
+Pi_numerator = T_plus - T_minus
+
+if display_progress:
+    print('computing spectral projector denominator Pi_denom = 2I + T_minus^(2^k) + T_plus^(2^k)')
+Pi_denominator = (T_minus + T_plus).add_identity(s=2.0)
+
+if display_progress:
+    print('inverting spectral projector denominator: Pi_denom^-1')
+iPi_denominator = Pi_denominator.inv(rtol=rtol, atol=atol)
+
+if display_progress:
+    print('computing spectral projector: Pi_num * Pi_denom^-1')
+Pi = hpro.h_mul(Pi_numerator, iPi_denominator,
+                rtol=rtol, atol=atol,
+                display_progress=display_progress)
+
+if display_progress:
+    print('computing A_plus = Pi * A')
+A_plus = hpro.h_mul(Pi, A_hmatrix,
                      rtol=rtol, atol=atol,
                      display_progress=display_progress).sym()
 
-A_plus = A_hmatrix + (A_minus * -1.0)
+# A_plus = A_hmatrix + (A_minus * -1.0)
 # A_plus = A_hmatrix + (A_minus * -2.0)
 
-T_dense = build_dense_matrix_from_matvecs(T.matvec, T.shape[1])
+# T_dense = build_dense_matrix_from_matvecs(T.matvec, T.shape[1])
+Pi_dense = build_dense_matrix_from_matvecs(Pi.matvec, Pi.shape[1])
+ee_Pi, uu_Pi = sla.eigh(Pi_dense)
 
 A_plus_dense = build_dense_matrix_from_matvecs(A_plus.matvec, A_plus.shape[1])
 ee_plus, uu_plus = sla.eigh(A_plus_dense)
 print('np.min(ee_plus)=', np.min(ee_plus))
 
-plt.figure()
-plt.plot(ee_plus)
+ray_plus_dense = np.sum(uu_A_dense * (A_plus_dense @ uu_A_dense), axis=0)
 
+plt.figure()
+# plt.plot(ee_plus)
+plt.plot(ray_plus_dense)
+plt.plot(ee_A_dense)
+
+
+plt.figure()
+plt.plot(ee_A_dense,ee_Pi)
+
+####################################
+################################3
+############################33
+
+rtol = hpro.default_rtol
+atol = hpro.default_atol
+
+A_hmatrix = A_hmatrix.sym()
+
+ee_LM, _ = spla.eigsh(A_hmatrix.as_linear_operator(), k=1, which='LM')
+lambda_max = np.max(ee_LM)
+
+ee_SA, _ = spla.eigsh(A_hmatrix.as_linear_operator(), k=1, which='SA')
+lambda_min = np.min(ee_SA)
+
+B_hmatrix = (A_hmatrix * (0.5 / np.abs(lambda_min))).add_identity().inv(rtol=1e-4, atol=1e-6)
+
+# B_dense = build_dense_matrix_from_matvecs(B_hmatrix.matvec, B_hmatrix.shape[1])
+# ee_B, uu_B = sla.eigh(B_dense)
+
+aX = 1.0
+bX = 3.0
+kX = 3
+
+TX = B_hmatrix.copy()
+TX = (TX * 2.0).add_identity(s=-(bX + aX)) * (1.0 / (bX - aX))
+
+for ii in range(kX):
+    TX = hpro.h_mul(TX, TX,
+              rtol=rtol, atol=atol,
+              display_progress=display_progress)
+
+###
+
+rtol = hpro.default_rtol
+atol = hpro.default_atol
+
+A_hmatrix = A_hmatrix.sym()
+
+ee_LM, _ = spla.eigsh(A_hmatrix.as_linear_operator(), k=1, which='LM')
+lambda_max = np.max(ee_LM)
+
+a = 0.0
+b = 1.5 * lambda_max
+k = 6
+
+T = A_hmatrix.copy()
+T = (T * 2.0).add_identity(s=-(b + a)) * (1.0 / (b - a))
+
+for ii in range(k):
+    T = hpro.h_mul(T, T,
+                   rtol=rtol, atol=atol,
+                   display_progress=display_progress)
+
+Pi_plus = T.add_identity().inv(rtol=rtol, atol=atol)
+
+Pi = (Pi_plus * 2.0).add_identity(s=-1.0)
+
+A_plus = hpro.h_mul(Pi, A_hmatrix,
+                    rtol=rtol, atol=atol,
+                    display_progress=display_progress).sym()
+
+A_dense = build_dense_matrix_from_matvecs(A_hmatrix.matvec, A_hmatrix.shape[1])
+ee, uu = sla.eigh(A_dense)
+
+A_plus_dense = build_dense_matrix_from_matvecs(A_plus.matvec, A_plus.shape[1])
+ee_plus, uu_plus = sla.eigh(A_plus_dense)
+
+ray_plus = np.sum(uu * (A_plus_dense @ uu), axis=0)
+
+plt.figure()
+plt.plot(ee)
+plt.plot(ee_plus)
+plt.plot(ray_plus)
+
+###
+
+rtol = hpro.default_rtol
+atol = hpro.default_atol
+
+A_hmatrix = A_hmatrix.sym()
+
+ee_SA, _ = spla.eigsh(A_hmatrix.as_linear_operator(), k=1, which='SA')
+lambda_min = np.min(ee_SA)
+
+a = 1.5*lambda_min
+b = 0.0
+k = 1
+
+T = A_hmatrix.copy()
+T = (T * 2.0).add_identity(s=-(b + a)) * (1.0 / (b - a))
+
+for ii in range(k):
+    T = hpro.h_mul(T, T,
+                   rtol=rtol, atol=atol,
+                   display_progress=display_progress)
+
+Pi_plus = T.add_identity().inv(rtol=rtol, atol=atol)
+
+Pi = (Pi_plus * (-2.0)).add_identity()
+
+A_plus = hpro.h_mul(Pi, A_hmatrix,
+                    rtol=rtol, atol=atol,
+                    display_progress=display_progress).sym()
+
+# A_dense = build_dense_matrix_from_matvecs(A_hmatrix.matvec, A_hmatrix.shape[1])
+# ee, uu = sla.eigh(A_dense)
+
+A_plus_dense = build_dense_matrix_from_matvecs(A_plus.matvec, A_plus.shape[1])
+ee_plus, uu_plus = sla.eigh(A_plus_dense)
+
+ray_plus = np.sum(uu * (A_plus_dense @ uu), axis=0)
+
+plt.figure()
+plt.plot(ee)
+plt.plot(ee_plus)
+plt.plot(ray_plus)
+
+
+####
+
+
+eeX, uuX = spla.eigsh(LinopWithCounter(B_hmatrix.as_linear_operator()), k=10, which='LM')
+
+
+B_dense = build_dense_matrix_from_matvecs(B_hmatrix.matvec, B_hmatrix.shape[1])
+ee_B, uu_B = sla.eigh(B_dense)
+
+k1 = 4
+# a1 = np.abs(lambda_min)
+a1 = 0.1*lambda_max
+b1 = 2.0*lambda_max
+
+T1_scaling_at_zero = 1. / (1. + ((2.0 * 0.0 - (b1 + a1)) / (b1 - a1)) ** (2 ** k1))
+print('T1_scaling_at_zero=', T1_scaling_at_zero)
+
+T1 = A_hmatrix.copy()
+T1 = (T1 * 2.0).add_identity(s=-(b1 + a1)) * (1.0 / (b1 - a1))
+
+if display_progress:
+    print('computing T_plus^(2^k)')
+for ii in range(k_plus):
+    if display_progress:
+        print('computing T_plus^(2^' + str(ii + 1) + ') = T_plus^(2^' + str(ii) + ') * T_plus^(2^' + str(ii) + ')')
+    T_plus = hpro.h_mul(T_plus, T_plus,
+              rtol=rtol, atol=atol,
+              display_progress=display_progress)
+
+if display_progress:
+    print('computing T_minus^(2^k)')
+for ii in range(k_minus):
+    if display_progress:
+        print('computing T_minus^(2^' + str(ii + 1) + ') = T_minus^(2^' + str(ii) + ') * T_minus^(2^' + str(ii) + ')')
+    T_minus = hpro.h_mul(T_minus, T_minus,
+              rtol=rtol, atol=atol,
+              display_progress=display_progress)
+
+#
+#
+#
 #
 
 A_pch0 = A_pch_nonsym.sym()
@@ -164,20 +469,6 @@ plt.plot(ee0)
 #
 
 ee_T, _ = sla.eigh(T_dense)
-
-#
-
-class LinopWithCounter:
-    def __init__(me, linop):
-        me.counter = 0
-        me.linop = linop
-        me.shape = linop.shape
-        me.dtype = linop.dtype
-
-    def matvec(me, x):
-        me.counter+=1
-        print('counter=', me.counter)
-        return me.linop.matvec(x)
 
 #
 
