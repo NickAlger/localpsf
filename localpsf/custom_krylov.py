5bf5fc8276f57dcc21307201894f288a7041878b
NickAlger <nalger225@gmail.com>
2023-03-07 17:43:48 -0600

    wrote custom gmres function that can terminate when negative direction is encountered

---
+++ localpsf/custom_krylov.py
@@ -0,0 +1,139 @@
+import numpy as np
+import scipy.linalg as sla
+import typing as typ
+import numpy.typing as npt
+
+
+def positive_gmres(apply_A: typ.Callable[[np.ndarray], np.ndarray],  # R^N -> R^N
+                   b: np.ndarray,  # shape=(N,)
+                   x0: np.ndarray=None,  # shape=(N,)
+                   apply_M: typ.Callable[[np.ndarray], np.ndarray]=None,  # R^N -> R^N
+                   solve_M: typ.Callable[[np.ndarray], np.ndarray]=None,  # R^N -> R^N
+                   rtol: float=1e-10,
+                   max_iter=50,
+                   terminate_negative_direction=True,
+                   display=True,
+                   callback: typ.Callable[[np.ndarray, float, float], None]=None, # callback(xk, norm_r, xt_A_x)
+                   ) -> np.ndarray:
+    '''Modified from Algorithm 6.9 in Saad iterative methods book page 172. No restarts.'''
+    assert(len(b.shape) == 1)
+    N = len(b)
+
+    if solve_M is None or apply_M is None:
+        assert(solve_M is None)
+        assert(apply_M is None)
+        apply_M = lambda x: x
+        solve_M = lambda x: x
+
+    def printmaybe(stuff):
+        if display:
+            print(stuff)
+
+    if callback is None:
+        callback = lambda xk, nr, xAx: None
+
+    norm_b = np.linalg.norm(b)
+
+    if x0 is None:
+        x0 = np.zeros(N)
+        Ax0 = np.zeros(N)
+    else:
+        assert(x0.shape == (N,))
+        Ax0 = apply_A(x0)
+    r0 = solve_M(b - Ax0)
+
+    beta = np.linalg.norm(r0)
+
+    V_big = np.zeros((N, max_iter + 1))
+    V_big[:,0] = r0 / beta
+
+    norm_r = np.linalg.norm(b - Ax0)
+    xt_A_x = np.dot(x0, Ax0)
+    callback(x0, norm_r, xt_A_x)
+    printmaybe('initial guess: norm_r/norm_b=' + str(norm_r / norm_b) + ', xt_A_x=' + str(xt_A_x))
+
+    x = x0
+    H_big = np.zeros((max_iter + 1, max_iter))
+    for jj in range(max_iter):
+        w = solve_M(apply_A(V_big[:, jj]))
+        H_big[:jj+1, jj] = w @ V_big[:, :jj+1]
+        w = w - V_big[:, :jj+1] @ H_big[:jj+1, jj]
+        h = np.linalg.norm(w)
+        H_big[jj+1, jj] = h
+        if h < 1e-15*norm_b:
+            printmaybe('GMRES success: good breakdown')
+            break
+        V_big[:,jj+1] = w / h
+
+        H = H_big[:jj+2, :jj+1] # shape=(j+1, j)
+        V = V_big[:, :jj+1] # shape=(N, j)
+
+        e = np.zeros(jj+2)
+        e[0] = 1.0
+
+        y = sla.lstsq(H, beta*e)[0]
+        # norm_r = np.linalg.norm(beta*e - H @ y)
+
+        x_prop = x0 + V @ y
+        # norm_r_true = np.linalg.norm(solve_M(b - apply_A(x_prop)))
+        # print('norm_r=', norm_r)
+        # print('norm_r_true=', norm_r_true)
+
+        Ax_prop = Ax0 + apply_M(V_big[:, :jj+2] @ (H @ y))
+        norm_r = np.linalg.norm(b - Ax_prop)
+        # Ax_true = apply_A(x_prop)
+        #
+        # print('Ax error:', np.linalg.norm(Ax_prop - Ax_true)/np.linalg.norm(Ax_true))
+
+        xt_A_x = np.dot(x_prop, Ax_prop)
+        # xt_A_x_true = np.dot(x_prop, apply_A(x_prop))
+        # print('xt_A_x=', xt_A_x)
+        # print('xt_A_x_true=', xt_A_x_true)
+
+        printmaybe('jj=' + str(jj) + ', norm_r/norm_b=' + str(norm_r / norm_b) + ', xt_A_x=' + str(xt_A_x))
+        callback(x_prop, norm_r, xt_A_x)
+
+        if terminate_negative_direction:
+            if xt_A_x < 0:
+                if jj == 0:
+                    x = x_prop
+                printmaybe('GMRES terminated: negative direction encountered')
+                break
+
+        x = x_prop
+
+        if norm_r < rtol * norm_b:
+            printmaybe('GMRES success: rtol achieved')
+            break
+
+    return x
+
+
+
+if __name__ == "__main__":
+    N = 100
+    A = np.random.randn(N,N)
+    x_true = np.random.randn(N)
+    apply_A = lambda p: A @ p
+    b = apply_A(x_true)
+
+    M = A + 0.1 * np.random.randn(*A.shape)
+    iM = np.linalg.inv(M)
+    apply_M = lambda p: M @ p
+    solve_M = lambda p: iM @ p
+
+    xx = []
+    all_norm_r = []
+    all_xAx = []
+    def callback(xk, nr, xAx):
+        xx.append(xk)
+        all_norm_r.append(nr)
+        all_xAx.append(xAx)
+
+
+    x = positive_gmres(apply_A, b, max_iter=100, apply_M=apply_M, solve_M=solve_M, callback=callback)
+    err = np.linalg.norm(x - x_true) / np.linalg.norm(x_true)
+    relres = np.linalg.norm(b - apply_A(x)) / np.linalg.norm(b)
+    xt_A_x = np.dot(x, apply_A(x))
+    print('err=', err, ', relres=', relres, ', xt_A_x=', xt_A_x)
+
--- localpsf/positive_definite_modifications.py
+++ localpsf/positive_definite_modifications.py
@@ -1,4 +1,6 @@
 import numpy as np
+import typing as typ
+import scipy.linalg as sla
 import scipy.sparse.linalg as spla
 
 
@@ -81,3 +83,215 @@ class WoodburyObject: # (A + U*C*V) * x = b
         y = me.solve_A(b)
         return y - me.solve_A(np.dot(me.U, me.solve_capacitance_matrix(np.dot(me.V, y))))
 
+
+def eig_coeff(eig: float,  # bad eigenvalue A*u = eig*B*u
+              tau: float,  # tolerance in (0,1)
+              method='zero',
+              ) -> float:  # correction coefficient
+    if method == 'thresh':  # eig + 1 + c = tau
+        c = tau - eig - 1.0
+    elif method == 'zero':  # eig + 1 + c = 1
+        c = -eig
+    elif method == 'flip':  # eig + 1 + c = 1 + |eig|
+        c = np.abs(eig) - eig
+    return c
+
+
+def bad_generalized_eigs(apply_A: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                         apply_B: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                         solve_B: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                         N: int,
+                         tau: float = 0.5,  # tolerance in (0,1)
+                         chunk_size: int = 5,
+                         max_rank: int = 50,
+                         display: bool = False,
+                         ) -> typ.Tuple[np.ndarray,  # U=[u0, u1, ...]: shape=(N,k)
+                                        np.ndarray,  # V=[B@u0, B@u1, ...]: shape=(N,k)
+                                        np.ndarray]:  # eigs=[e0, e1, ...]: shape=(N,))
+    '''Finds geigs A@ui=ei*B@ui such that ui.T@(A + B)@ui < tau.
+
+    A is symmetric indefinite
+    B is symmetric positive definite
+    A.shape=B.shape=(N,N)
+    apply_A(x) = A @ x
+    apply_B(x) = B @ x
+    solve_B(apply_B(x)) = apply_B(solve_B(x)) = x
+    '''
+    Bop = spla.LinearOperator((N, N), matvec=apply_B)
+    iBop = spla.LinearOperator((N, N), matvec=solve_B)
+    uu: typ.List[np.ndarray] = []
+    vv: typ.List[np.ndarray] = []
+    ee: typ.List[float] = []
+    while len(ee) < max_rank:
+        if ee:
+            cc = np.array([eig_coeff(ei, tau, method='flip') for ei in ee])
+            VT = np.array(vv)
+            extra_term = lambda x: VT.T @ (cc * (VT @ x.reshape(-1)))
+        else:
+            extra_term = lambda x: 0.0 * x
+
+        def apply_modified_A(x: np.ndarray) -> np.ndarray:
+            return apply_A(x) + extra_term(x)
+
+        modified_A_linop = spla.LinearOperator((N, N), matvec=apply_modified_A)
+
+        if display:
+            print('computing chunk, chunk_size=', chunk_size)
+        ee_chunk, U_chunk = spla.eigsh(modified_A_linop, k=chunk_size,
+                                       M=Bop, Minv=iBop, which='SA')
+        done = False
+        for ii in range(len(ee_chunk)):
+            if display:
+                print('eig=', ee_chunk[ii], ', tau-1.0=', tau - 1.0)
+            if ee_chunk[ii] < tau - 1.0:
+                ee.append(ee_chunk[ii])
+                uu.append(U_chunk[:, ii])
+                vv.append(apply_B(U_chunk[:, ii]))
+            if ee_chunk[ii] >= tau - 1.0:
+                #                 if display:
+                #                     print('Done.')
+                done = True
+        if done:
+            break
+
+    U = np.array(vv).T
+    V = np.array(vv).T
+    eigs = np.array(ee)
+    return U, V, eigs
+
+
+def bad_eig_correction(apply_A: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                       apply_B: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                       solve_B: typ.Callable[[np.ndarray], np.ndarray],  # shape=(N,N)
+                       N: int,
+                       tau: float = 0.5,  # tolerance in (0,1)
+                       chunk_size: int = 5,
+                       max_rank: int = 50,
+                       display: bool = False,
+                       method='flip',
+                       ) -> typ.Tuple[np.ndarray,  # V: shape=(N,k)
+                                      np.ndarray]:  # cc: shape=(N,))
+    '''Finds low rank correction for A+B to reduce impact of indefiniteness of A.
+
+    A is symmetric indefinite
+    B is symmetric positive definite
+    A.shape=B.shape=(N,N)
+    apply_A(x) = A @ x
+    apply_B(x) = B @ x
+    solve_B(apply_B(x)) = apply_B(solve_B(x)) = x
+
+    Bad eigenpairs (ui, ei) of A @ ui = ei B @ ui satisfy
+        ui.T @ (A + B) @ ui < tau * ui.T @ B @ ui = tau
+
+    We form low rank correction
+        A + B + V * diag(cc) * V.T
+    such that:
+    1) V * diag(cc) * V.T is diagonalized by the generalized eigenvectors of (A,B)
+    2) For all good (ui, ei), we have
+        ui.T @ (A + B + V * diag(cc) * V.T) @ ui = ei
+    3) For all bad (ui, ei), we have:
+        ui.T @ (A + B + V * diag(cc) * V.T) @ ui = tau    (method='thresh')
+    or:
+        ui.T @ (A + B + V * diag(cc) * V.T) @ ui = 1.0  (method='zero')
+    or:
+        ui.T @ (A + B + V * diag(cc) * V.T) @ ui = |ei|   (method='flip')
+
+    In:
+        import numpy as np
+        import scipy.linalg as sla
+
+        N = 250
+        tau = 0.75
+
+        tmp = np.random.randn(N,N)
+        A = 0.5 * (tmp.T + tmp) + 10.0*np.eye(N)
+
+        tmp2 = np.random.randn(N,N)
+        B = sla.sqrtm(tmp2.T @ tmp2)
+
+        # Flip bad geigs with brute force:
+        ee, P = sla.eigh(A, B)
+        ee_flip = np.zeros(len(ee))
+        for ii in range(len(ee)):
+            if ee[ii] + 1.0 < tau:
+                ee_flip[ii] = 1.0 + np.abs(ee[ii])
+            else:
+                ee_flip[ii] = 1.0 + ee[ii]
+
+        # Flip bad geigs using bad_eig_correction():
+        apply_A = lambda x: A @ x
+        apply_B = lambda x: B @ x
+        Binv = np.linalg.inv(B) # yeah yeah..
+        solve_B = lambda x: Binv @ x
+
+        V, cc = bad_eig_correction(apply_A, apply_B, solve_B, N, tau=tau, display=True)
+
+        # Check correctness:
+        M_flip = A + B + V @ np.diag(cc) @ V.T
+        rayleigh = P.T @ M_flip @ P
+        err_offdiagonal = np.linalg.norm(rayleigh - np.diag(np.diag(rayleigh)))
+        print('err_offdiagonal=', err_offdiagonal)
+
+        ee_flip2 = np.diag(rayleigh)
+        err_eigs = np.linalg.norm(ee_flip2 - ee_flip)
+        print('err_eigs=', err_eigs)
+    Out:
+        computing chunk, chunk_size= 5
+        eig= -2.1169078852087293 , tau-1.0= -0.25
+        eig= -1.5119785357654094 , tau-1.0= -0.25
+        eig= -1.4801150091446882 , tau-1.0= -0.25
+        eig= -1.4142264301418281 , tau-1.0= -0.25
+        eig= -1.185010446973965 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -1.1327176696065981 , tau-1.0= -0.25
+        eig= -1.0916879589192034 , tau-1.0= -0.25
+        eig= -1.063314361843518 , tau-1.0= -0.25
+        eig= -1.0303695336441607 , tau-1.0= -0.25
+        eig= -0.9709878075990548 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.8495969511308968 , tau-1.0= -0.25
+        eig= -0.8291943636018237 , tau-1.0= -0.25
+        eig= -0.8018363279042096 , tau-1.0= -0.25
+        eig= -0.7830844793418776 , tau-1.0= -0.25
+        eig= -0.7403524793086638 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.735298765100338 , tau-1.0= -0.25
+        eig= -0.6636932846854344 , tau-1.0= -0.25
+        eig= -0.6383936273234821 , tau-1.0= -0.25
+        eig= -0.6144255514080768 , tau-1.0= -0.25
+        eig= -0.5795848362962359 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.574869852579649 , tau-1.0= -0.25
+        eig= -0.5432625433613644 , tau-1.0= -0.25
+        eig= -0.5190436794411728 , tau-1.0= -0.25
+        eig= -0.5131552033100857 , tau-1.0= -0.25
+        eig= -0.47017694259277565 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.46084034665978996 , tau-1.0= -0.25
+        eig= -0.4440927670801954 , tau-1.0= -0.25
+        eig= -0.43208877326967887 , tau-1.0= -0.25
+        eig= -0.41496521814721526 , tau-1.0= -0.25
+        eig= -0.3969132555532589 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.37869939226993904 , tau-1.0= -0.25
+        eig= -0.35074103027590675 , tau-1.0= -0.25
+        eig= -0.3451955295364068 , tau-1.0= -0.25
+        eig= -0.3113603526636124 , tau-1.0= -0.25
+        eig= -0.3079814029501603 , tau-1.0= -0.25
+        computing chunk, chunk_size= 5
+        eig= -0.2973717331807069 , tau-1.0= -0.25
+        eig= -0.28115048847204194 , tau-1.0= -0.25
+        eig= -0.2746033707682397 , tau-1.0= -0.25
+        eig= -0.253627512604388 , tau-1.0= -0.25
+        eig= -0.22870982686316774 , tau-1.0= -0.25
+        err_offdiagonal= 8.583318868457877e-11
+        err_eigs= 2.6052423115160627e-11
+
+
+    '''
+    U, V, eigs = bad_generalized_eigs(apply_A, apply_B, solve_B,
+                                      N, tau=tau, chunk_size=chunk_size,
+                                      max_rank=max_rank, display=display)
+
+    cc = np.array([eig_coeff(ei, tau, method=method) for ei in list(eigs)])
+    return V, cc
