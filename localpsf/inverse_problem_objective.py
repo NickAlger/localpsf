d1c374a033ab50b433e3ef9a8e0e54b2617744bf
NickAlger <nalger225@gmail.com>
2023-04-18 18:15:01 -0500

    spectrum looks good with smoothed version

--- localpsf/inverse_problem_objective.py
+++ localpsf/inverse_problem_objective.py
@@ -6,7 +6,7 @@ from functools import cached_property
 from .assertion_helpers import *
 
 @dataclass
-class InverseProblem:
+class InverseProblemObjective:
     misfit: typ.Any
     regularization: typ.Any
     regularization_parameter: float
@@ -41,35 +41,45 @@ class InverseProblem:
         assert_gt(new_a_reg, 0.0)
         me.regularization_parameter = new_a_reg
 
-    def cost_triple(me) -> typ.Tuple[float, float, float]:
+    def misfit_cost(me) -> float:
         Jd: float = me.misfit.misfit()
+        return Jd
+
+    def regularization_cost(me) -> float:
         Jr: float = me.regularization.cost(me.get_optimization_variable(), me.regularization_parameter)
+        return Jr
+
+    def cost_triple(me) -> typ.Tuple[float, float, float]:
+        Jd: float = me.misfit_cost()
+        Jr: float = me.regularization_cost()
         J: float = Jd + Jr
         return J, Jd, Jr
 
-    def gradient_triple(me) -> typ.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def misfit_gradient(me) -> np.ndarray:
         gd = me.misfit.gradient()
         assert_equal(gd.shape, (me.N,))
+        return gd
+
+    def regularization_gradient(me) -> np.ndarray:
         gr = me.regularization.gradient(me.get_optimization_variable(), me.regularization_parameter)
         assert_equal(gr.shape, (me.N,))
+        return gr
+
+    def gradient_triple(me) -> typ.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        gd = me.misfit_gradient()
+        gr = me.regularization_gradient()
         g = gd + gr
         return g, gd, gr
 
     def apply_hessian_triple(me, p: np.ndarray) -> typ.Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        assert_equal(p.shape, (me.N,))
-        Hd_p = me.misfit.apply_hessian(p)
-        assert_equal(Hd_p.shape, (me.N,))
-        Hr_p = me.regularization.apply_hessian(p, me.get_optimization_variable(), me.regularization_parameter)
-        assert_equal(Hr_p.shape, (me.N,))
+        Hd_p = me.apply_misfit_hessian(p)
+        Hr_p = me.apply_regularization_hessian(p)
         H_p = Hd_p + Hr_p
         return H_p, Hd_p, Hr_p
 
     def apply_gauss_newton_hessian_triple(me, p: np.ndarray) -> typ.Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        assert_equal(p.shape, (me.N,))
-        Hdgn_p = me.misfit.apply_gauss_newton_hessian(p)
-        assert_equal(Hdgn_p.shape, (me.N,))
-        Hr_p = me.regularization.apply_hessian(p, me.get_optimization_variable(), me.regularization_parameter)
-        assert_equal(Hr_p.shape, (me.N,))
+        Hdgn_p = me.apply_misfit_gauss_newton_hessian(p)
+        Hr_p = me.apply_regularization_hessian(p)
         Hgn_p = Hdgn_p + Hr_p
         return Hgn_p, Hdgn_p, Hr_p
 
@@ -78,6 +88,24 @@ class InverseProblem:
 
     def gradient(me) -> np.ndarray:
         return me.gradient_triple()[0]
+
+    def apply_misfit_hessian(me, p: np.ndarray) -> np.ndarray:
+        assert_equal(p.shape, (me.N,))
+        Hd_p = me.misfit.apply_hessian(p)
+        assert_equal(Hd_p.shape, (me.N,))
+        return Hd_p
+
+    def apply_misfit_gauss_newton_hessian(me, p: np.ndarray) -> np.ndarray:
+        assert_equal(p.shape, (me.N,))
+        Hdgn_p = me.misfit.apply_gauss_newton_hessian(p)
+        assert_equal(Hdgn_p.shape, (me.N,))
+        return Hdgn_p
+
+    def apply_regularization_hessian(me, p: np.ndarray) -> np.ndarray:
+        assert_equal(p.shape, (me.N,))
+        Hr_p = me.regularization.apply_hessian(p, me.get_optimization_variable(), me.regularization_parameter)
+        assert_equal(Hr_p.shape, (me.N,))
+        return Hr_p
 
     def apply_hessian(me, p: np.ndarray) -> np.ndarray:
         return me.apply_hessian_triple(p)[0]
