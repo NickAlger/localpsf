1bd3d349eadc4f21204e7d33b57e064ab152b572
NickAlger <nalger225@gmail.com>
2023-05-04 15:46:52 -0500

    working on advection diffusion problem

--- numerical_examples/advection_diffusion/adv_deflation.py
+++ numerical_examples/advection_diffusion/adv_deflation.py
@@ -226,6 +226,242 @@ def checkerboard_function(nx, ny, Vh, smoothing_time=2e-4):
     return checker_func
 
 
+####
+
+dt = 0.1
+t_init = 0.
+obs_coords = Vh.tabulate_dof_coordinates()  # observe everywhere
+print("Number of observation points: {0}".format(obs_coords.shape[0]))
+
+
+@dataclass
+class AdvInverseProblemUnregularized:
+    mesh_and_function_space: AdvMeshAndFunctionSpace
+    problem: TimeDependentAD
+    misfit: SpaceTimePointwiseStateObservation
+    true_initial_condition: np.ndarray
+    obs: np.ndarray
+    true_obs: np.ndarray
+
+    @cached_property
+    def N(me) -> int:
+        return me.mesh_and_function_space.Vh.dim()
+
+    def noise(me) -> np.ndarray:
+        return me.obs - me.true_obs
+
+    def update_noise(me, new_noise: np.ndarray) -> None:
+        me.obs[:] = me.true_obs + new_noise
+
+    def misfit_cost(me, m_vec: np.ndarray) -> float:
+        [u, m, p] = me.problem.generate_vector()
+        m[:] = m_vec.copy()
+        me.problem.solveFwd(u, [u, m, p])
+        me.problem.solveAdj(p, [u, m, p])
+        total_cost, reg_cost, misfit_cost = me.problem.cost([u, m, p])
+        return misfit_cost
+
+    def misfit_gradient(me, m_vec: np.ndarray) -> np.ndarray:
+        [u, m, p] = me.problem.generate_vector()
+        m[:] = m_vec.copy()
+        me.problem.solveFwd(u, [u, m, p])
+        me.problem.solveAdj(p, [u, m, p])
+        mg = me.problem.generate_vector(PARAMETER)
+        grad_norm = me.problem.evalGradientParameter([u, m, p], mg, misfit_only=True)
+        return mg[:].copy()
+
+    def apply_misfit_hessian(me, x: np.ndarray) -> np.ndarray:
+        x_petsc = dl.Function(me.mesh_and_function_space.Vh).vector()
+        x_petsc[:] = x.copy()
+        y_petsc = dl.Function(me.mesh_and_function_space.Vh).vector()
+        me.H.mult(x_petsc, y_petsc)
+        return y_petsc[:].copy()
+
+    def get_misfit_hessian_impulse_response(me, location: typ.Union[int, np.ndarray]) -> np.ndarray:  # impulse response at kth dof location
+        if isinstance(location, int):
+            k = location
+        else:
+            assert(location.shape == (me.mesh_and_function_space.dof_coords.shape[1],))
+            k = me.mesh_and_function_space.find_nearest_dof(location)
+        assert(k < me.N)
+        ek = np.zeros(me.N)
+        ek[k] = 1.0
+        return me.apply_misfit_hessian(ek / me.mesh_and_function_space.mass_lumps) / me.mesh_and_function_space.mass_lumps
+
+
+    def __init__(me, kappa, t_final, gamma):
+        print('AdvectionDiffusionMisfitStuff: kappa=', kappa, ', t_final=', t_final)
+        me.kappa = kappa
+        me.t_final = t_final
+        me._gamma = gamma
+
+        me.REG = BiLaplacianRegularization(me._gamma, prior_correlation_length,
+                                           dl.Function(Vh), prior_mean_func)
+
+        me.simulation_times = np.arange(t_init, me.t_final + .5 * dt, dt)
+        me.observation_times = np.array([me.t_final])
+
+        me.misfit = SpaceTimePointwiseStateObservation(Vh,
+                                                       me.observation_times,
+                                                       obs_coords)
+
+        fake_gamma = 1.0
+        fake_delta = 1.0
+
+        fake_prior = BiLaplacianPrior(Vh, fake_gamma, fake_delta, robin_bc=True)
+
+        me.problem = TimeDependentAD(mesh, [Vh, Vh, Vh], fake_prior,
+                                     me.misfit, me.simulation_times,
+                                     wind_velocity, True, kappa=me.kappa)
+
+        me.utrue = me.problem.generate_vector(STATE)
+        x = [me.utrue, true_initial_condition, None]
+        me.problem.solveFwd(x[STATE], x)
+        me.misfit.observe(x, me.misfit.d)
+
+        me.true_obs_numpy = me.misfit.d.data[0][:].copy()
+
+        MAX = me.misfit.d.norm("linf", "linf")
+        noise_std_dev = rel_noise * MAX
+        parRandom.normal_perturb(noise_std_dev, me.misfit.d)
+        me.misfit.noise_variance = noise_std_dev * noise_std_dev
+
+        me.obs_numpy = me.misfit.d.data[0][:].copy()
+
+        me.noise_numpy = me.true_obs_numpy - me.obs_numpy
+        me.noise_norm = np.linalg.norm(me.noise_numpy)
+        print('noise_norm=', me.noise_norm)
+
+        uf_petsc = dl.Function(Vh).vector()
+        x[0].retrieve(uf_petsc, t_final)
+
+        me.utrue_final = dl.Function(Vh)
+        me.utrue_final.vector()[:] = uf_petsc
+
+        me.H = ReducedHessian(me.problem, misfit_only=True)
+
+        me.apply_Hr = me.REG.apply_hessian_petsc
+        me.solve_Hr = me.REG.solve_hessian_petsc
+
+    @property
+    def gamma(me):
+        return me._gamma
+
+    def update_gamma(me, new_gamma):
+        me._gamma = gamma
+        me.REG.update_gamma(new_gamma)
+
+    def eval_misfit_objective(me, m_petsc):
+        [u, m, p] = me.problem.generate_vector()
+        m[:] = m_petsc
+        me.problem.solveFwd(u, [u, m, p])
+        me.problem.solveAdj(p, [u, m, p])
+        total_cost, reg_cost, misfit_cost = me.problem.cost([u, m, p])
+        return misfit_cost
+
+    def eval_misfit_gradient(me, m_petsc, return_gradnorm=False):
+        [u, m, p] = me.problem.generate_vector()
+        m[:] = m_petsc
+        me.problem.solveFwd(u, [u, m, p])
+        me.problem.solveAdj(p, [u, m, p])
+        mg = me.problem.generate_vector(PARAMETER)
+        grad_norm = me.problem.evalGradientParameter([u, m, p], mg, misfit_only=True)
+        if return_gradnorm:
+            return mg, grad_norm
+        else:
+            return mg
+
+    def apply_Hd(me, x_petsc):
+        y_petsc = dl.Function(Vh).vector()
+        me.H.mult(x_petsc, y_petsc)
+        return y_petsc
+
+    def apply_Hd_numpy(me, x_numpy):
+        x_petsc = dl.Function(Vh).vector()
+        x_petsc[:] = x_numpy
+        return me.apply_Hd(x_petsc)[:]
+
+    def eval_regularization_objective(me, m_petsc):
+        me.REG.parameter.vector()[:] = m_petsc
+        return me.REG.cost()
+
+    def eval_objective(me, m_petsc):
+        return me.eval_misfit_objective(m_petsc) + me.eval_regularization_objective(m_petsc)
+
+    def eval_regularization_gradient(me, m_petsc):
+        me.REG.parameter.vector()[:] = m_petsc
+        return me.REG.gradient_petsc()
+
+    def eval_gradient(me, m_petsc):
+        return me.eval_misfit_gradient(m_petsc) + me.eval_regularization_gradient(m_petsc)
+
+    def eval_gradient_numpy(me, m_numpy):
+        m_petsc = dl.Function(Vh).vector()
+        m_petsc[:] = m_numpy
+        return me.eval_gradient(m_petsc)[:]
+
+    def apply_H(me, x_petsc):
+        return me.apply_Hd(x_petsc) + me.apply_Hr(x_petsc)
+
+    def apply_H_numpy(me, x_numpy):
+        x_petsc = dl.Function(Vh).vector()
+        x_petsc[:] = x_numpy
+        return me.apply_H(x_petsc)[:]
+
+    def apply_Hr_numpy(me, x_numpy):
+        x_petsc = dl.Function(Vh).vector()
+        x_petsc[:] = x_numpy
+        return me.apply_Hr(x_petsc)[:]
+
+    def solve_Hr_numpy(me, x_numpy):
+        x_petsc = dl.Function(Vh).vector()
+        x_petsc[:] = x_numpy
+        return me.solve_Hr(x_petsc)[:]
+
+    def get_impulse_response(me, k: int, lumped_mass=False
+                             ) -> np.ndarray:  # impulse response at kth dof location
+        ek = np.zeros(Vh.dim())
+        ek[k] = 1.0
+        if lumped_mass:
+            return solve_ML_numpy(me.apply_Hd_numpy(solve_ML_numpy(ek)))
+        else:
+            return solve_M_numpy(me.apply_Hd_numpy(solve_M_numpy(ek)))
+
+    def compute_morozov_discrepancy(me, m_star_numpy):
+        m_star = dl.Function(Vh).vector()
+        m_star[:] = m_star_numpy
+        u_star = me.problem.generate_vector(STATE)
+        x = [u_star, m_star, None]
+        me.problem.solveFwd(x[STATE], x)
+        new_obs = me.misfit.d.copy()
+        new_obs.zero()
+        me.misfit.observe(x, new_obs)
+        return np.linalg.norm(new_obs.data[0][:] - me.obs_numpy)
+
+
+ADP = AdvectionDiffusionProblem(kappa, t_final, gamma)
+
+cmap = 'gray'  # 'binary_r' #'gist_gray' #'binary' # 'afmhot'
+
+plt.figure(figsize=(5, 5))
+cm = dl.plot(ADP.utrue_final, cmap=cmap)
+plt.axis('off')
+cm.set_clim(0.0, 1.0)
+# cm.extend = 'both'
+plt.colorbar(cm, fraction=0.046, pad=0.04)
+plt.gca().set_aspect('equal')
+plt.show()
+
+plt.set_cmap('viridis')
+
+plt.figure(figsize=(5, 5))
+p = np.array([0.25, 0.75])
+impulse_response0 = dl.Function(Vh)
+impulse_response0.vector()[:] = ADP.get_impulse_response(find_nearest_dof(p))
+cm = dl.plot(impulse_response0)
+plt.colorbar(cm)
+plt.title('impulse response near ' + str(p))
+
 
 ic_func = checkerboard_function(num_checkers_x, num_checkers_y, Vh)
 true_initial_condition = ic_func.vector()
