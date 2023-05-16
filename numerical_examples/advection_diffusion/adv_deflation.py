c7ba5751b344f972a81a35f6d0a4941ac888a894
NickAlger <nalger225@gmail.com>
2023-05-16 13:00:25 -0500

    working on advection diffusion results

--- numerical_examples/advection_diffusion/adv_deflation.py
+++ numerical_examples/advection_diffusion/adv_deflation.py
@@ -22,6 +22,29 @@ import hlibpro_python_wrapper as hpro
 from localpsf.product_convolution_kernel import ProductConvolutionKernel
 from localpsf.product_convolution_hmatrix import make_hmatrix_from_kernel
 
+
+def compute_threshold_crossings(y: np.ndarray, thresholds: np.ndarray) -> np.ndarray:
+    '''out[ii]=k where k is first index at which y[k] < threshold[ii].
+
+    In:
+        compute_threshold_crossings(np.logspace(-1, -5, 20), np.array([10, 1e-2, 1e-4, 1e-20]))
+    Out:
+        array([ 0,  5, 15])
+    '''
+    assert(len(y.shape) == 1)
+    assert(len(thresholds.shape) == 1)
+    crossings = np.ones(len(thresholds), dtype=int) * np.nan
+    for ii, threshold in enumerate(thresholds):
+        good_inds = np.argwhere(y < threshold).reshape(-1)
+        if len(good_inds) > 0:
+            crossing = np.min(good_inds)
+            crossings[ii] = crossing
+        # print('good_inds=', good_inds)
+        # print('y=', y)
+        # print('threshold=', threshold)
+        # print('crossing=', crossing)
+    return crossings
+
 fig_dpi = 200
 
 save_dir = localpsf_root / 'numerical_examples' / 'advection_diffusion' / 'data'
@@ -35,37 +58,115 @@ save_dir_str = str(save_dir)
 num_checkers = 8
 
 all_noise_levels = [0.05, 0.1, 0.2]
-all_kappa = list(np.logspace(-4, -3, 3))
+all_kappa = list(np.logspace(-4, -3, 3)) # 1e-3, 3.16e-4, 1e-4
 all_t_final = [0.5, 1.0, 2.0]
 all_num_batches = [1, 5, 25]
+
+krylov_thresholds = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+
+impulse_points = np.array([[0.1, 0.8],
+                           [0.75, 0.25],
+                           [0.3, 0.6]])
 
 np.savetxt(save_dir_str + '/all_noise_levels.txt', np.array(all_noise_levels))
 np.savetxt(save_dir_str + '/all_kappa.txt', np.array(all_kappa))
 np.savetxt(save_dir_str + '/all_t_final.txt', np.array(all_t_final))
-
-# X_impulse, Y_impulse = np.meshgrid(np.linspace(0.0, 1.0, 11), np.linspace(0.0, 1.0, 11))
-# impulse_points = np.array([X_impulse.reshape(-1), Y_impulse.reshape(-1)]).T.copy()
-impulse_points = np.array([[0.1, 0.8]])
+np.savetxt(save_dir_str + '/all_num_batches.txt', np.array(all_num_batches))
+np.savetxt(save_dir_str + '/krylov_thresholds.txt', np.array(krylov_thresholds))
 np.savetxt(save_dir_str + '/impulse_points.txt', impulse_points)
 
-krylov_threshold = 1e-6
 
-krylov_iters_none = np.ones((len(all_noise_levels),
-                             len(all_kappa),
-                             len(all_t_final)), dtype=int) * np.nan
+#### MAKE INITIAL CONDITION PLOT ####
 
-krylov_iters_reg = np.ones((len(all_noise_levels),
-                            len(all_kappa),
-                            len(all_t_final)), dtype=int) * np.nan
+ADV = adv.make_adv_universe(all_noise_levels[0], all_kappa[0], all_t_final[0],
+                            num_checkers_x=num_checkers,
+                            num_checkers_y=num_checkers)
 
-krylov_iters_psf = np.ones((len(all_num_batches),
-                            len(all_noise_levels),
-                            len(all_kappa),
-                            len(all_t_final)), dtype=int) * np.nan
+plt.figure()
+cm = dl.plot(ADV.true_initial_condition_func, cmap='gray')
+plt.colorbar(cm)
+plt.axis('off')
+plt.title('True initial condition')
+plt.savefig(save_dir_str + '/true_initial_condition.png', dpi=fig_dpi, bbox_inches='tight')
+
+dl.File(save_dir_str + "/true_initial_condition.pvd") << ADV.true_initial_condition_func
+plt.close()
+
+
+#### MAKE VELOCITY PLOT ####
+
+ADV.wind_object.plot_velocity()
+plt.title('Wind velocity')
+plt.savefig(save_dir_str + '/wind_velocity.png', dpi=fig_dpi, bbox_inches='tight')
+
+dl.File(save_dir_str + "/wind_velocity.pvd") << ADV.wind_object.velocity
+plt.close()
+
+#### INITIALIZE DATA ARRAYS ####
+
+newton_krylov_iters_none = np.ones((len(all_noise_levels),
+                                    len(all_kappa),
+                                    len(all_t_final),
+                                    len(krylov_thresholds)), dtype=int) * np.nan
+
+newton_krylov_iters_reg = np.ones((len(all_noise_levels),
+                                   len(all_kappa),
+                                   len(all_t_final),
+                                   len(krylov_thresholds)), dtype=int) * np.nan
+
+newton_krylov_iters_psf = np.ones((len(all_num_batches),
+                                   len(all_noise_levels),
+                                   len(all_kappa),
+                                   len(all_t_final),
+                                   len(krylov_thresholds)), dtype=int) * np.nan
+
+####
+
+randn_krylov_iters_none = np.ones((len(all_noise_levels),
+                                   len(all_kappa),
+                                   len(all_t_final),
+                                   len(krylov_thresholds)), dtype=int) * np.nan
+
+randn_krylov_iters_reg = np.ones((len(all_noise_levels),
+                                  len(all_kappa),
+                                  len(all_t_final),
+                                  len(krylov_thresholds)), dtype=int) * np.nan
+
+randn_krylov_iters_psf = np.ones((len(all_num_batches),
+                                  len(all_noise_levels),
+                                  len(all_kappa),
+                                  len(all_t_final),
+                                  len(krylov_thresholds)), dtype=int) * np.nan
+
+####
+
+geigs_none = np.ones((len(all_noise_levels),
+                      len(all_kappa),
+                      len(all_t_final),
+                      ADV.N)) * np.nan
+
+geigs_reg = np.ones((len(all_noise_levels),
+                     len(all_kappa),
+                     len(all_t_final),
+                     ADV.N)) * np.nan
+
+geigs_psf = np.ones((len(all_num_batches),
+                     len(all_noise_levels),
+                     len(all_kappa),
+                     len(all_t_final),
+                     ADV.N)) * np.nan
+
+####
 
 all_areg_morozov = np.ones((len(all_noise_levels),
-                             len(all_kappa),
-                             len(all_t_final))) * np.nan
+                            len(all_kappa),
+                            len(all_t_final))) * np.nan
+
+all_noise_norms = np.ones((len(all_noise_levels),
+                           len(all_kappa),
+                           len(all_t_final))) * np.nan
+
+####
 
 for ii, ns in enumerate(all_noise_levels):
     for jj, kp in enumerate(all_kappa):
@@ -80,27 +181,6 @@ for ii, ns in enumerate(all_noise_levels):
             ADV = adv.make_adv_universe(ns, kp, tf,
                                         num_checkers_x=num_checkers,
                                         num_checkers_y=num_checkers)
-
-            #### INITIAL CONDITION PLOT ####
-            if (ii==0) and (jj==0) and (kk==0):
-                plt.figure()
-                cm = dl.plot(ADV.true_initial_condition_func, cmap='gray')
-                plt.colorbar(cm)
-                plt.axis('off')
-                plt.title('True initial condition')
-                plt.savefig(save_dir_str + '/true_initial_condition.png', dpi=fig_dpi, bbox_inches='tight')
-
-                dl.File(save_dir_str + "/true_initial_condition.pvd") << ADV.true_initial_condition_func
-
-            #### NOISY OBSERVATIONS PLOT ####
-            plt.figure()
-            cm = dl.plot(ADV.obs_func, cmap='gray')
-            plt.colorbar(cm)
-            plt.axis('off')
-            plt.title('Noisy observations' + str(tf))
-            plt.savefig(save_dir_str + '/noisy_observations'+id_str+'.png', dpi=fig_dpi, bbox_inches='tight')
-
-            dl.File(save_dir_str + '/noisy_observations'+id_str+'.pvd') << ADV.obs_func
 
             #### IMPULSE RESPONSE PLOTS ####
             if ii == 0: # impulse responses don't depend on noise
@@ -123,9 +203,21 @@ for ii, ns in enumerate(all_noise_levels):
                     plt.clf()
                 plt.close()
 
+            #### PLOT OBSERVATIONS ####
+            plt.figure()
+            cm = dl.plot(ADV.obs_func, cmap='gray')
+            plt.colorbar(cm)
+            plt.axis('off')
+            plt.title('noisy observations')
+            plt.savefig(save_dir_str + '/noisy_observations'+id_str+'.png', dpi=fig_dpi, bbox_inches='tight')
+
+            dl.File(save_dir_str + '/noisy_observations'+id_str+'.pvd') << ADV.obs_func
+            plt.close()
+
             #### COMPUTE MOROZOV REGULARIZATION PARAMETER ####
+            areg_initial_guess = 1e-5
             areg_morozov, seen_aregs, seen_discrepancies = ADV.compute_morozov_regularization_parameter(
-                1e-5, morozov_options={'morozov_rtol' : 1e-4})
+                areg_initial_guess, morozov_options={'morozov_rtol' : 1e-3})
 
             inds = np.argsort(seen_aregs)
             seen_aregs = seen_aregs[inds]
@@ -133,6 +225,7 @@ for ii, ns in enumerate(all_noise_levels):
 
             print('areg_morozov=', areg_morozov)
             all_areg_morozov[ii,jj,kk] = areg_morozov
+            all_noise_norms[ii,jj,kk] = ADV.noise_norm
 
             np.save(save_dir_str + '/all_areg_morozov', all_areg_morozov)
             np.savetxt(save_dir_str + '/seen_aregs' + id_str + '.txt', seen_aregs)
@@ -145,6 +238,7 @@ for ii, ns in enumerate(all_noise_levels):
             plt.ylabel('Morozov discrepancy')
             plt.title('noise_level=' + str(ns))
             plt.savefig(save_dir_str + '/morozov' + id_str + '.png', dpi=fig_dpi, bbox_inches='tight')
+            plt.close()
 
             #### SOLVE TO TIGHT TOLERANCE AND PLOT RECONSTRUCTION ####
             print('Solving inverse problem to tight tol with areg_morozov')
@@ -166,8 +260,9 @@ for ii, ns in enumerate(all_noise_levels):
             plt.savefig(save_dir_str + '/reconstructed_initial_condition'+id_str+'.png', dpi=fig_dpi, bbox_inches='tight')
 
             dl.File(save_dir_str + '/reconstructed_initial_condition'+id_str+'.pvd') << mstar
+            plt.close()
 
-            #### KRYLOV CONVERGENCE PLOTS ####
+            #### KRYLOV CONVERGENCE AND PRECONDITIONED SPECTRUM PLOTS ####
             print('Making Krylov convergence plots')
             M_reg_matvec = lambda x: ADV.regularization.solve_hessian(x, m0, areg_morozov)
             M_reg_linop = spla.LinearOperator((ADV.N, ADV.N), matvec=M_reg_matvec)
@@ -178,13 +273,18 @@ for ii, ns in enumerate(all_noise_levels):
             np.savetxt(save_dir_str + '/errs_none' + id_str + '.txt', errs_none)
             np.savetxt(save_dir_str + '/errs_reg' + id_str + '.txt', errs_reg)
 
-            iter_count_none = np.min(np.argwhere(np.array(errs_none) < krylov_threshold))
-            iter_count_reg = np.min(np.argwhere(np.array(errs_reg) < krylov_threshold))
+            newton_crossings_none = compute_threshold_crossings(np.array(errs_none), np.array(krylov_thresholds))
+            newton_crossings_reg = compute_threshold_crossings(np.array(errs_reg), np.array(krylov_thresholds))
 
-            krylov_iters_none[ii, jj, kk] = iter_count_none
-            krylov_iters_reg[ii, jj, kk] = iter_count_reg
-            np.save(save_dir_str + '/krylov_iters_none', krylov_iters_none)
-            np.save(save_dir_str + '/krylov_iters_reg', krylov_iters_reg)
+            newton_krylov_iters_none[ii, jj, kk] = newton_crossings_none
+            newton_krylov_iters_reg[ii, jj, kk] = newton_crossings_reg
+
+            np.save(save_dir_str + '/newton_krylov_iters_none', newton_krylov_iters_none)
+            np.save(save_dir_str + '/newton_krylov_iters_reg', newton_krylov_iters_reg)
+
+            print('krylov_thresholds=', krylov_thresholds)
+            print('newton_crossings_none=', newton_crossings_none)
+            print('newton_crossings_reg=', newton_crossings_reg)
 
             #
 
@@ -220,7 +320,9 @@ for ii, ns in enumerate(all_noise_levels):
 
                 iter_count_psf = np.min(np.argwhere(np.array(errs_psf) < krylov_threshold))
                 krylov_iters_psf[ll, ii, jj, kk] = iter_count_psf
-                np.savetxt(save_dir_str + '/krylov_iters_psf', krylov_iters_psf)
+                np.save(save_dir_str + '/krylov_iters_psf', krylov_iters_psf)
+
+
 
 
 plt.figure()
