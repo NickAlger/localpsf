246cf35b7ef0ea0e31beec9f1ecc81f88f480543
NickAlger <nalger225@gmail.com>
2024-02-05 14:06:09 -0600

    frog example is good

--- numerical_examples/blur/blur_example.py
+++ numerical_examples/blur/blur_example.py
@@ -5,8 +5,8 @@ import localpsf.localpsf_cg1_lumped as lpsf1
 import scipy.linalg as sla
 
 
-nx = 50
-ny = 50
+nx = 63
+ny = 63
 mesh = dl.UnitSquareMesh(nx, ny)
 Vh = dl.FunctionSpace(mesh, 'CG', 1)
 dof_coords = Vh.tabulate_dof_coordinates()
@@ -29,114 +29,71 @@ def ricker(
     t_squared_over_sigma_squared = np.einsum('ai,ai->a', pp, np.einsum('ij,aj->ai', inv_Sigma, pp))
     G = vol / (2.0 * np.pi * np.sqrt(np.linalg.det(Sigma))) * np.exp(-0.5 * t_squared_over_sigma_squared)
 
-    # cos_t_over_sigma = np.cos(np.sqrt(t_squared_over_sigma_squared))
     cos_x_over_sigma = np.cos(pp[:,0] / (np.sqrt(Sigma[0,0]) / 2.0))
     sin_y_over_sigma = np.sin(pp[:,1] / (np.sqrt(Sigma[1,1]) / 2.0))
     return (1.0 + a * cos_x_over_sigma * sin_y_over_sigma) * G
-    # return (1.0 + a * cos_x_over_sigma) * G
-    # return cos_x_over_sigma * G
-    # return cos_t_over_sigma * G
-    # return (1.0 + a * cos_t_over_sigma) * G
-    # return (1.0 - a * t_squared_over_sigma_squared) * G
 
-# xx = np.vstack([X.reshape(-1), Y.reshape(-1)]).T
 
-length_scaling = 0.25 # <--- Tucker: experiment with this
+length_scaling = 1.0 # <--- Tucker: experiment with this
 vol = 1.0
 mu = np.array([0.5, 0.5])
 Sigma = length_scaling * np.array([[0.01, 0.0], [0.0, 0.0025]]) # 0.25*np.array([[0.01, 0.0], [0.0, 0.0025]])
-a_small = 0.0
-a_medium = 1.1 # 0.1
-a_large = 5.0 # <--- Tucker: use a=1
+a = 1.0
 
-rr_small = ricker(dof_coords, vol, mu, Sigma, a_small)
-rr_medium = ricker(dof_coords, vol, mu, Sigma, a_medium)
-rr_large = ricker(dof_coords, vol, mu, Sigma, a_large)
+rr = ricker(dof_coords, vol, mu, Sigma, a)
 
-
-f1_small = dl.Function(Vh)
-f1_small.vector()[:] = rr_small
-
-f1_medium = dl.Function(Vh)
-f1_medium.vector()[:] = rr_medium
-
-f1_large = dl.Function(Vh)
-f1_large.vector()[:] = rr_large
-
-plt.figure()
-cm = dl.plot(f1_small)
-plt.colorbar(cm)
-plt.title('f1_small')
-
-plt.figure()
-cm = dl.plot(f1_medium)
-plt.colorbar(cm)
-plt.title('f1_medium')
-
-plt.figure()
-cm = dl.plot(f1_large)
-plt.colorbar(cm)
-plt.title('f1_large')
+f1 = dl.Function(Vh)
+f1.vector()[:] = rr
 
 num_pts_1d = 500
-# coords_1d = np.array([np.linspace(0, 1, num_pts_1d), mu[1]*np.ones(num_pts_1d)]).T # horizontal line
-# coords_1d = np.array([np.linspace(0, 1, num_pts_1d), np.linspace(0, 1, num_pts_1d)]).T # 45 degree line
 coords_1d = np.array([mu[0]*np.ones(num_pts_1d), np.linspace(0, 1, num_pts_1d)]).T # vertical line
 
 plt.figure()
-cm = dl.plot(f1_medium)
+cm = dl.plot(f1)
 plt.colorbar(cm)
-plt.title('f1_medium')
+plt.title('f1')
 plt.plot(coords_1d[:,0], coords_1d[:,1], '.r')
 
-rr_1d_small = ricker(coords_1d, vol, mu, Sigma, a_small)
-rr_1d_medium = ricker(coords_1d, vol, mu, Sigma, a_medium)
-rr_1d_large = ricker(coords_1d, vol, mu, Sigma, a_large)
+rr_1d = ricker(coords_1d, vol, mu, Sigma, a)
 
 plt.figure()
-# plt.plot(coords_1d[:,0], rr_1d_small)
-plt.plot(rr_1d_small)
-plt.title('rr_1d_small')
+plt.plot(rr_1d)
+plt.title('rr_1d')
 
-plt.figure()
-# plt.plot(coords_1d[:,0], rr_1d_medium)
-plt.plot(rr_1d_medium)
-plt.title('rr_1d_medium')
-
-plt.figure()
-# plt.plot(coords_1d[:,0], rr_1d_large)
-plt.plot(rr_1d_large)
-plt.title('rr_1d_large')
-
-Ker_small = np.zeros((Vh.dim(), Vh.dim()))
+Ker = np.zeros((Vh.dim(), Vh.dim()))
 for ii in range(Vh.dim()):
-    Ker_small[:,ii] = ricker(dof_coords, vol, dof_coords[ii,:], Sigma, a_small)
+    Ker[:,ii] = ricker(dof_coords, vol, dof_coords[ii,:], Sigma, a)
 
-Ker_medium = np.zeros((Vh.dim(), Vh.dim()))
-for ii in range(Vh.dim()):
-    Ker_medium[:,ii] = ricker(dof_coords, vol, dof_coords[ii,:], Sigma, a_medium)
 
-Ker_large = np.zeros((Vh.dim(), Vh.dim()))
-for ii in range(Vh.dim()):
-    Ker_large[:,ii] = ricker(dof_coords, vol, dof_coords[ii,:], Sigma, a_large)
+p1 = np.array([0.2, 0.55])
+p2 = np.array([0.5, 0.65])
 
-ii1=713
-ii2=1504
-plt.matshow(Ker_medium, cmap='binary')
-# plt.matshow(np.sqrt(np.abs(Ker_medium)), cmap='binary')
-plt.plot([ii1, ii1], [0, Ker_medium.shape[0]], 'r')
-plt.plot([ii2, ii2], [0, Ker_medium.shape[0]], 'r')
-plt.ylim([Ker_medium.shape[0], 0])
-plt.title('true kernel medium')
+ii1 = np.argmin(np.linalg.norm(dof_coords - p1.reshape((1,-1)), axis=1))
+ii2 = np.argmin(np.linalg.norm(dof_coords - p2.reshape((1,-1)), axis=1))
+
+# ii1=713
+# ii2=2404
+plt.matshow(Ker, cmap='binary')
+# plt.plot([ii1, ii1], [0, Ker.shape[0]], 'k', linestyle=(0,(1,1)))
+plt.plot([ii1, ii1], [0, Ker.shape[0]], 'k', linestyle='dotted')
+plt.plot([ii2, ii2], [0, Ker.shape[0]], 'k', linestyle='dotted')
+plt.ylim([Ker.shape[0], 0])
+plt.gca().set_xticks([])
+plt.gca().set_yticks([])
+plt.savefig('frog_kernel_matrix_a1.png', bbox_inches='tight')
+
 
 
 phi = dl.Function(Vh)
-phi.vector()[:] = Ker_medium[:,ii1].copy()
+phi.vector()[:] = Ker[:,ii1].copy()
 plt.figure()
 cm = dl.plot(phi, cmap='binary')
-# plt.colorbar(cm)
-# plt.title('phi')
-plt.plot(dof_coords[ii1,0], dof_coords[ii1,1], '.r')
+# plt.plot(dof_coords[ii1,0], dof_coords[ii1,1], 'sk')
+
+phi = dl.Function(Vh)
+phi.vector()[:] = Ker[:,ii2].copy()
+plt.figure()
+cm = dl.plot(phi, cmap='binary')
 
 phi = dl.Function(Vh)
 phi.vector()[:] = Ker_medium[:,ii2].copy()
