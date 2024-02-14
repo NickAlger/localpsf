43a2d52237a9e5a1e4ab59394e0d4e10b238fb21
NickAlger <nalger225@gmail.com>
2024-02-14 16:22:33 -0600

    fixed sqrt bug when calculating GLR FRO rank

--- numerical_examples/blur/blur_length_scale_experiments.py
+++ numerical_examples/blur/blur_length_scale_experiments.py
@@ -18,6 +18,8 @@ all_length_scalings = [1.0 / (t**2) for t in [1.0, 2.0, 3.0]]
 all_error_levels = [0.2, 0.1, 0.05]
 # length_scaling = 1./(3.0**2) #1.0
 
+do_PSF = False
+
 np.savetxt('all_length_scalings.txt', all_length_scalings)
 np.savetxt('all_error_levels.txt', all_error_levels)
 
@@ -42,7 +44,7 @@ for length_scaling in all_length_scalings:
 
     all_glr_ranks_fro = []
     for rtol in all_error_levels:
-        glr_num_fro = np.sum((norm_ker_fro**2 - np.cumsum(ss**2)) > rtol * norm_ker_fro**2)
+        glr_num_fro = np.sum((norm_ker_fro**2 - np.cumsum(ss**2)) > rtol**2 * norm_ker_fro**2)
         all_glr_ranks_fro.append(glr_num_fro)
         print('rtol=', rtol, ', glr_num_fro=', glr_num_fro)
     all_all_glr_ranks_fro.append(all_glr_ranks_fro)
@@ -54,62 +56,64 @@ for length_scaling in all_length_scalings:
         print('rtol=', rtol, ', glr_num_2=', glr_num_2)
     all_all_glr_ranks_2.append(all_glr_ranks_2)
 
-    #### PSF
-
-    psf_object = lpsf1.make_psf_fenics(
-        lambda X: H @ X,
-        lambda X: H.T @ X,
-        Vh, Vh,
-        mass_lumps, mass_lumps,
-        num_initial_batches=0,
-        tau=3.0, display=True,
-        num_neighbors=10
-    )
-
-    # print('Making row and column cluster trees')
-    # ct = hpro.build_cluster_tree_from_pointcloud(dof_coords, cluster_size_cutoff=32)
-    #
-    # print('Making block cluster trees')
-    # bct = hpro.build_block_cluster_tree(ct, ct, admissibility_eta=2.0)
-
-    all_num_batches = []
-    all_num_impulses = []
-    all_fro_errors = []
-    for num_batches in range(max_batches):
-        psf_object.add_impulse_response_batch()
-        num_batches = psf_object.psf_object.impulse_response_batches.num_batches
-        num_impulses = psf_object.psf_object.impulse_response_batches.num_sample_points
-
-        # H_psf_hmatrix, Ker_psf_hmatrix = psf_object.construct_hmatrices(bct)
-        #
-        # Ker_psf = np.zeros(Ker.shape)
-        # for ii in range(Ker_psf.shape[1]):
-        #     ei = np.zeros(Ker_psf.shape[1])
-        #     ei[ii] = 1.0
-        #     Ker_psf[:,ii] = Ker_psf_hmatrix * ei
-
-        Ker_psf = psf_object.psf_object.psf_kernel.cpp_object.eval_integral_kernel_block(dof_coords.T, dof_coords.T)
-        err_psf = np.linalg.norm(Ker_psf - Ker) / np.linalg.norm(Ker)
-        print('err_psf=', err_psf)
-        print('num_batches=', num_batches)
-        print('num_impulses=', num_impulses)
-
-        all_fro_errors.append(err_psf)
-        all_num_batches.append(num_batches)
-        all_num_impulses.append(num_impulses)
-
-        if err_psf < np.min(all_error_levels):
-            break
-
-    all_all_num_batches.append(all_num_batches)
-    all_all_num_impulses.append(all_num_impulses)
-    all_all_fro_errors.append(all_fro_errors)
-
-    np.savetxt('all_num_batches_L='     +str(length_scaling)+'.txt', all_num_batches)
-    np.savetxt('all_num_impulses_L='    +str(length_scaling)+'.txt', all_num_impulses)
-    np.savetxt('all_fro_errors_L='      +str(length_scaling)+'.txt', all_fro_errors)
     np.savetxt('frog_singular_values_L='+str(length_scaling)+'.txt', ss)
     np.savetxt('all_glr_ranks_fro_L='   +str(length_scaling)+'.txt', all_glr_ranks_fro)
     np.savetxt('all_glr_ranks_2_L='     +str(length_scaling)+'.txt', all_glr_ranks_2)
+
+    #### PSF
+    if do_PSF:
+        psf_object = lpsf1.make_psf_fenics(
+            lambda X: H @ X,
+            lambda X: H.T @ X,
+            Vh, Vh,
+            mass_lumps, mass_lumps,
+            num_initial_batches=0,
+            tau=3.0, display=True,
+            num_neighbors=10
+        )
+
+        # print('Making row and column cluster trees')
+        # ct = hpro.build_cluster_tree_from_pointcloud(dof_coords, cluster_size_cutoff=32)
+        #
+        # print('Making block cluster trees')
+        # bct = hpro.build_block_cluster_tree(ct, ct, admissibility_eta=2.0)
+
+        all_num_batches = []
+        all_num_impulses = []
+        all_fro_errors = []
+        for num_batches in range(max_batches):
+            psf_object.add_impulse_response_batch()
+            num_batches = psf_object.psf_object.impulse_response_batches.num_batches
+            num_impulses = psf_object.psf_object.impulse_response_batches.num_sample_points
+
+            # H_psf_hmatrix, Ker_psf_hmatrix = psf_object.construct_hmatrices(bct)
+            #
+            # Ker_psf = np.zeros(Ker.shape)
+            # for ii in range(Ker_psf.shape[1]):
+            #     ei = np.zeros(Ker_psf.shape[1])
+            #     ei[ii] = 1.0
+            #     Ker_psf[:,ii] = Ker_psf_hmatrix * ei
+
+            Ker_psf = psf_object.psf_object.psf_kernel.cpp_object.eval_integral_kernel_block(dof_coords.T, dof_coords.T)
+            err_psf = np.linalg.norm(Ker_psf - Ker) / np.linalg.norm(Ker)
+            print('err_psf=', err_psf)
+            print('num_batches=', num_batches)
+            print('num_impulses=', num_impulses)
+
+            all_fro_errors.append(err_psf)
+            all_num_batches.append(num_batches)
+            all_num_impulses.append(num_impulses)
+
+            if err_psf < np.min(all_error_levels):
+                break
+
+        all_all_num_batches.append(all_num_batches)
+        all_all_num_impulses.append(all_num_impulses)
+        all_all_fro_errors.append(all_fro_errors)
+
+        np.savetxt('all_num_batches_L='     +str(length_scaling)+'.txt', all_num_batches)
+        np.savetxt('all_num_impulses_L='    +str(length_scaling)+'.txt', all_num_impulses)
+        np.savetxt('all_fro_errors_L='      +str(length_scaling)+'.txt', all_fro_errors)
+
 
 
